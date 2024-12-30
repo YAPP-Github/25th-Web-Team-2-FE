800ffdfe5f45f190a4c77f7203a885f67598043b
rbgksqkr <rbgks1937@naver.com>
2024-12-30 21:54:29 +0900

    fix: msw module not found 오류 해결

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,24 @@ const nextConfig = {
   compiler: {
     emotion: true,
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // next server build => ignore msw/browser
+      if (Array.isArray(config.resolve.alias)) {
+        config.resolve.alias.push({ name: 'msw/browser', alias: false });
+      } else {
+        config.resolve.alias['msw/browser'] = false;
+      }
+    } else {
+      // browser build => ignore msw/node
+      if (Array.isArray(config.resolve.alias)) {
+        config.resolve.alias.push({ name: 'msw/node', alias: false });
+      } else {
+        config.resolve.alias['msw/node'] = false;
+      }
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
