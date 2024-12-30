7c1f6c162218c0116807a8b9fad76d56ee8abf07
rbgksqkr <rbgks1937@naver.com>
2024-12-30 22:22:25 +0900

    refactor: 불필요한 worker 호출 방지

--- src/mocks/MSWProvider.tsx
+++ src/mocks/MSWProvider.tsx
@@ -1,12 +1,16 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const MSWProvider = ({ children }: { children: React.ReactNode }) => {
   const [isMSWReady, setIsMSWReady] = useState(false);
+  const isStartedRef = useRef(false);
 
   useEffect(() => {
     const init = async () => {
+      if (isStartedRef.current) return;
+      isStartedRef.current = true;
+
       const initMSW = await import('./index').then((res) => res.initMSW);
       await initMSW();
       setIsMSWReady(true);
