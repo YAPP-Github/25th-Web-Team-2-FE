12286d5ec8ccb6b99e2519f16c16d6cd5df0dec0
rbgksqkr <rbgks1937@naver.com>
2025-01-04 16:45:07 +0900

    [YS-74] feat: Footer 컴포넌트 구현

--- src/components/layout/DefaultLayout.tsx
+++ src/components/layout/DefaultLayout.tsx
@@ -1,11 +1,13 @@
 import Header from '@/components/Header/Header';
 import React, { PropsWithChildren } from 'react';
+import Footer from '../Footer/Footer';
 
 const DefaultLayout = ({ children }: PropsWithChildren) => {
   return (
     <>
       <Header />
       {children}
+      <Footer />
     </>
   );
 };
