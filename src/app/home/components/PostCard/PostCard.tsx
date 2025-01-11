3da17c0ad4907081cf5f7774654d75e487b67349
rbgksqkr <rbgks1937@naver.com>
2025-01-12 00:20:37 +0900

    [YS-85] feat: 카드 클릭했을 때 게시글과 링크 연결

--- src/app/home/components/PostCard/PostCard.tsx
+++ src/app/home/components/PostCard/PostCard.tsx
@@ -15,6 +15,7 @@ import {
 } from './PostCard.styles';
 import Icon from '@/components/Icon';
 import theme from '@/styles/theme';
+import Link from 'next/link';
 
 interface PostCardProps {
   post: Post;
@@ -22,41 +23,45 @@ interface PostCardProps {
 
 const PostCard = ({ post }: PostCardProps) => {
   return (
-    <li
-      css={postCardLayout}
-      style={{
-        backgroundColor: post.isContacted ? theme.colors.field01 : theme.colors.field01,
-        cursor: post.isContacted ? 'not-allowed' : 'pointer',
-      }}
-    >
-      <div css={postHeader}>
-        <div css={postCardHeader}>
-          <span css={postLocation}>{post.place}</span>
-          <div css={postCardRightHeader}>
-            <Icon icon="Eye" width={18} />
-            <span css={postViews}>{post.views}</span>
+    <li>
+      <Link
+        href={`/post/${post.id}`}
+        key={post.id}
+        css={postCardLayout}
+        style={{
+          backgroundColor: post.isContacted ? theme.colors.field01 : theme.colors.field01,
+          cursor: post.isContacted ? 'not-allowed' : 'pointer',
+        }}
+      >
+        <div css={postHeader}>
+          <div css={postCardHeader}>
+            <span css={postLocation}>{post.place}</span>
+            <div css={postCardRightHeader}>
+              <Icon icon="Eye" width={18} />
+              <span css={postViews}>{post.views}</span>
+            </div>
           </div>
+          <h3 css={postTitle}>{post.title}</h3>
         </div>
-        <h3 css={postTitle}>{post.title}</h3>
-      </div>
-      <div>
-        {post.isContacted ? (
-          <div css={contactedPostTag}>
-            <span>모집 완료</span>
-          </div>
-        ) : (
-          <>
-            <div css={postRewardContainer}>
-              <span css={announceText}>보상</span>
-              <span css={postReward}>{post.reward}</span>
+        <div>
+          {post.isContacted ? (
+            <div css={contactedPostTag}>
+              <span>모집 완료</span>
             </div>
-            <div css={postRewardContainer}>
-              <span css={announceText}>일시</span>
-              <span css={postDate}>{post.testDate}</span>
-            </div>
-          </>
-        )}
-      </div>
+          ) : (
+            <>
+              <div css={postRewardContainer}>
+                <span css={announceText}>보상</span>
+                <span css={postReward}>{post.reward}</span>
+              </div>
+              <div css={postRewardContainer}>
+                <span css={announceText}>일시</span>
+                <span css={postDate}>{post.testDate}</span>
+              </div>
+            </>
+          )}
+        </div>
+      </Link>
     </li>
   );
 };
