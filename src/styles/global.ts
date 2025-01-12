43dea15635b7ec1f50ce23fe4b9bd94687aef757
rbgksqkr <rbgks1937@naver.com>
2025-01-12 22:16:01 +0900

    [YS-135] design: 스타일 적용 편의성을 위해 body 태그 공통 배경색 제거

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,7 +1,6 @@
 import { css } from '@emotion/react';
 
 import reset from './reset';
-import theme from './theme';
 
 const global = css`
   ${reset}
@@ -32,7 +31,6 @@ const global = css`
     display: block;
     font-family: var(--font-family);
     position: relative;
-    background-color: ${theme.colors.field02};
   }
 
   main {
