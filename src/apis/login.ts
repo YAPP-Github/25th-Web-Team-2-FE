ed0b9cfb1d004393da39336a62ee74c99c29fd80
rbgksqkr <rbgks1937@naver.com>
2025-01-17 00:48:34 +0900

    [YS-151] feat: 이메일 인증 API 구현

--- src/apis/login.ts
+++ src/apis/login.ts
@@ -2,6 +2,11 @@ import { API } from './config';
 
 import { API_URL } from '@/constants/url';
 
+interface UnivAuthCodeResponse {
+  isSuccess: boolean;
+  message: string;
+}
+
 interface LoginResponse {
   isRegistered: boolean;
   accessToken: string;
@@ -19,6 +24,18 @@ interface Member {
 
 export const googleLogin = async (code: string, role: string) => {
   const res = await API.post<LoginResponse>(API_URL.google(role), { authorizationCode: code });
+
+  return res.data;
+};
+
+export const sendUnivAuthCode = async (univEmail: string) => {
+  const res = await API.post<UnivAuthCodeResponse>(API_URL.send, { univEmail });
+
+  return res.data;
+};
+
+export const verifyUnivAuthCode = async (univEmail: string, inputCode: string) => {
+  const res = await API.post<UnivAuthCodeResponse>(API_URL.verify, { univEmail, inputCode });
 
   return res.data;
 };
--- src/constants/url.ts
+++ src/constants/url.ts
@@ -2,6 +2,8 @@ export const API_URL = {
   postList: '/v1/post',
   postArea: (area?: string) => `/v1/experiment-posts/counts${area ? `?area=${area}` : ''}`,
   google: (role: string) => `/v1/auth/login/google?role=${role}`,
+  send: 'v1/emails/send',
+  verify: 'v1/emails/verify',
 };
 
 export const MOCK_API_URL = {
