133ad79025e2db165b561753355fd2ef739800e0
rbgksqkr <rbgks1937@naver.com>
2025-01-18 03:42:20 +0900

    [YS-151] refactor: 구글 로그인 후 refreshToken 저장

--- src/app/login/hooks/useGoogleLoginMutation.ts
+++ src/app/login/hooks/useGoogleLoginMutation.ts
@@ -1,6 +1,7 @@
 import { useMutation } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
 
+import { API } from '@/apis/config';
 import { googleLogin } from '@/apis/login';
 
 const useGoogleLoginMutation = () => {
@@ -8,13 +9,16 @@ const useGoogleLoginMutation = () => {
 
   return useMutation({
     mutationFn: ({ code, role }: { code: string; role: string }) => googleLogin(code, role),
-    onSuccess: (data) => {
-      if (data.isRegistered) {
+    onSuccess: ({ isRegistered, accessToken, refreshToken, memberInfo }) => {
+      if (isRegistered) {
+        API.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+        sessionStorage.setItem('refreshToken', refreshToken);
+        sessionStorage.setItem('role', memberInfo.role);
         router.push('/');
         return;
       }
 
-      sessionStorage.setItem('email', data.memberInfo.oauthEmail);
+      sessionStorage.setItem('email', memberInfo.oauthEmail);
       router.push('/join');
     },
     onError: () => {
