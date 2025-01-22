8161c746c6fee6286001d8b14a9b685cc93e8f73
rbgksqkr <rbgks1937@naver.com>
2025-01-22 23:51:33 +0900

    [YS-196] refactor: 회원가입 시 공통 레이아웃 분리

--- src/app/join/components/JoinEmailStep/JoinEmailStep.tsx
+++ src/app/join/components/JoinEmailStep/JoinEmailStep.tsx
@@ -1,23 +1,13 @@
-import Image from 'next/image';
 import { useState } from 'react';
 import { useFormContext } from 'react-hook-form';
 
 import { joinContentContainer, nextButton } from './JoinEmailStep.styles';
 import useServiceAgreeCheck from '../../hooks/useServiceAgreeCheck';
-import {
-  contentContainer,
-  joinForm,
-  joinTitle,
-  progressBarContainer,
-  progressBarFill,
-  titleContainer,
-} from '../../JoinPage.styles';
+import { joinForm } from '../../JoinPage.styles';
 import { JoinParams } from '../../JoinPage.types';
 import JoinCheckboxContainer from './JoinCheckboxContainer/JoinCheckboxContainer';
 import JoinInput from '../JoinInput/JoinInput';
 import UnivAuthInput from './UnivAuthInput/UnivAuthInput';
-
-import Logo from '@/assets/images/logo.svg';
 
 interface JoinEmailStepProps {
   onNext: () => void;
@@ -45,60 +35,49 @@ const JoinEmailStep = ({ onNext }: JoinEmailStepProps) => {
   };
 
   return (
-    <>
-      <Image src={Logo} alt="로고" width={80} height={28} />
-      <div css={contentContainer}>
-        <div css={titleContainer}>
-          <h2 css={joinTitle}>연구자 회원가입</h2>
-          <div css={progressBarContainer}>
-            <div css={progressBarFill} style={{ width: '50%' }} />
-          </div>
-        </div>
-        <section css={joinForm}>
-          <div css={joinContentContainer}>
-            <JoinInput
-              name="oauthEmail"
-              control={control}
-              label="소셜 로그인 아이디"
-              value={oauthEmail}
-              placeholder="이메일 입력"
-              disabled
-              rules={{
-                required: '이메일을 입력해주세요',
-                pattern: {
-                  value: /^[^\s@ㄱ-ㅎㅏ-ㅣ가-힣]+@[^\s@ㄱ-ㅎㅏ-ㅣ가-힣]+\.[a-zA-Z]{2,}$/,
-                  message: '이메일 형식이 올바르지 않아요',
-                },
-              }}
-            />
-            <JoinInput
-              name="contactEmail"
-              control={control}
-              label="연락 받을 이메일"
-              placeholder="이메일 입력"
-              required
-              rules={{
-                required: '연락 받을 이메일을 입력해주세요',
-                pattern: {
-                  value: /^[^\s@ㄱ-ㅎㅏ-ㅣ가-힣]+@[^\s@ㄱ-ㅎㅏ-ㅣ가-힣]+\.[a-zA-Z]{2,}$/,
-                  message: '이메일 형식이 올바르지 않아요',
-                },
-              }}
-              tip="로그인 아이디와 달라도 괜찮아요"
-            />
-            <UnivAuthInput handleVerifyEmail={handleVerifyEmail} />
-            <JoinCheckboxContainer
-              serviceAgreeCheck={serviceAgreeCheck}
-              handleAllCheck={handleAllCheck}
-              handleChange={handleChangeCheck}
-            />
-          </div>
-          <button css={nextButton} onClick={handleNext} disabled={!allValid}>
-            다음
-          </button>
-        </section>
+    <section css={joinForm}>
+      <div css={joinContentContainer}>
+        <JoinInput
+          name="oauthEmail"
+          control={control}
+          label="소셜 로그인 아이디"
+          value={oauthEmail}
+          placeholder="이메일 입력"
+          disabled
+          rules={{
+            required: '이메일을 입력해주세요',
+            pattern: {
+              value: /^[^\s@ㄱ-ㅎㅏ-ㅣ가-힣]+@[^\s@ㄱ-ㅎㅏ-ㅣ가-힣]+\.[a-zA-Z]{2,}$/,
+              message: '이메일 형식이 올바르지 않아요',
+            },
+          }}
+        />
+        <JoinInput
+          name="contactEmail"
+          control={control}
+          label="연락 받을 이메일"
+          placeholder="이메일 입력"
+          required
+          rules={{
+            required: '연락 받을 이메일을 입력해주세요',
+            pattern: {
+              value: /^[^\s@ㄱ-ㅎㅏ-ㅣ가-힣]+@[^\s@ㄱ-ㅎㅏ-ㅣ가-힣]+\.[a-zA-Z]{2,}$/,
+              message: '이메일 형식이 올바르지 않아요',
+            },
+          }}
+          tip="로그인 아이디와 달라도 괜찮아요"
+        />
+        <UnivAuthInput handleVerifyEmail={handleVerifyEmail} />
+        <JoinCheckboxContainer
+          serviceAgreeCheck={serviceAgreeCheck}
+          handleAllCheck={handleAllCheck}
+          handleChange={handleChangeCheck}
+        />
       </div>
-    </>
+      <button css={nextButton} onClick={handleNext} disabled={!allValid}>
+        다음
+      </button>
+    </section>
   );
 };
 
--- src/app/join/page.tsx
+++ src/app/join/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Image from 'next/image';
 import { FormProvider, useForm } from 'react-hook-form';
 
 import JoinEmailStep from './components/JoinEmailStep/JoinEmailStep';
@@ -7,9 +8,19 @@ import JoinInfoStep from './components/JoinInfoStep/JoinInfoStep';
 import JoinSuccessStep from './components/JoinSuccessStep/JoinSuccessStep';
 import useFunnel from './hooks/useFunnel';
 import useJoinMutation from './hooks/useJoinMutation';
-import { joinLayout } from './JoinPage.styles';
+import {
+  contentContainer,
+  joinLayout,
+  joinTitle,
+  progressBarContainer,
+  progressBarFill,
+  titleContainer,
+} from './JoinPage.styles';
 import { JoinParams } from './JoinPage.types';
 import { getProvider } from './JoinPage.utils';
+
+import Logo from '@/assets/images/logo.svg';
+import { ROLE } from '@/constants/config';
 
 const STEP = {
   email: 'email',
@@ -19,6 +30,7 @@ const STEP = {
 
 export default function JoinPage() {
   const oauthEmail = sessionStorage.getItem('email') || '';
+  const role = sessionStorage.getItem('role') || '';
   const provider = getProvider(oauthEmail);
   const { mutate: join } = useJoinMutation();
 
@@ -35,7 +47,7 @@ export default function JoinPage() {
     },
   });
 
-  const { Funnel, setStep } = useFunnel(['email', 'info', 'success'] as const);
+  const { Funnel, step, setStep } = useFunnel(['email', 'info', 'success'] as const);
 
   const handleSubmit = () => {
     const formData = methods.getValues();
@@ -46,17 +58,28 @@ export default function JoinPage() {
   return (
     <FormProvider {...methods}>
       <section css={joinLayout}>
-        <Funnel>
-          <Funnel.Step name={STEP.email}>
-            <JoinEmailStep onNext={() => setStep(STEP.info)} />
-          </Funnel.Step>
-          <Funnel.Step name={STEP.info}>
-            <JoinInfoStep onNext={handleSubmit} />
-          </Funnel.Step>
-          <Funnel.Step name={STEP.success}>
-            <JoinSuccessStep name={methods.getValues('name')} />
-          </Funnel.Step>
-        </Funnel>
+        <Image src={Logo} alt="로고" width={80} height={28} />
+        <div css={contentContainer}>
+          <div css={titleContainer}>
+            <h2 css={joinTitle}>
+              {role === ROLE.researcher ? '연구자 회원가입' : '참여자 회원가입'}
+            </h2>
+            <div css={progressBarContainer}>
+              <div css={progressBarFill} style={{ width: step === STEP.email ? '50%' : '100%' }} />
+            </div>
+          </div>
+          <Funnel>
+            <Funnel.Step name={STEP.email}>
+              <JoinEmailStep onNext={() => setStep(STEP.info)} />
+            </Funnel.Step>
+            <Funnel.Step name={STEP.info}>
+              <JoinInfoStep onNext={handleSubmit} />
+            </Funnel.Step>
+            <Funnel.Step name={STEP.success}>
+              <JoinSuccessStep name={methods.getValues('name')} />
+            </Funnel.Step>
+          </Funnel>
+        </div>
       </section>
     </FormProvider>
   );
