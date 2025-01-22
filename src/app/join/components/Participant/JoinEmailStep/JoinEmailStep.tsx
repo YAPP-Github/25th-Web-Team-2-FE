921bbdee06920eaa4fb700e5134568fdc49c7ae7
rbgksqkr <rbgks1937@naver.com>
2025-01-23 02:22:46 +0900

    [YS-196] refactor: role에 맞게 EmailStep 분리

---
+++ src/app/join/components/Participant/JoinEmailStep/JoinEmailStep.tsx
@@ -0,0 +1,76 @@
+import { useFormContext } from 'react-hook-form';
+
+import JoinCheckboxContainer from '../../JoinEmailStep/JoinCheckboxContainer/JoinCheckboxContainer';
+
+import useServiceAgreeCheck from '@/app/join/hooks/useServiceAgreeCheck';
+import { ParticipantJoinParams } from '@/app/join/JoinPage.types';
+import { joinContentContainer, joinForm } from '@/app/join/JoinPage.styles';
+import JoinInput from '../../JoinInput/JoinInput';
+import { nextButton } from './JoinEmailStep.styles';
+
+interface JoinEmailStepProps {
+  onNext: () => void;
+}
+
+const JoinEmailStep = ({ onNext }: JoinEmailStepProps) => {
+  const oauthEmail = sessionStorage.getItem('email') || '';
+  const role = sessionStorage.getItem('role') || '';
+  const { control, trigger } = useFormContext<ParticipantJoinParams>();
+  const { serviceAgreeCheck, handleAllCheck, handleChangeCheck } = useServiceAgreeCheck(role);
+
+  const allValid = serviceAgreeCheck.isTermOfService && serviceAgreeCheck.isPrivacy;
+
+  const handleNext = async () => {
+    const isStepValid = await trigger(['oauthEmail', 'contactEmail']);
+    if (isStepValid) {
+      onNext();
+    }
+  };
+
+  return (
+    <section css={joinForm}>
+      <div css={joinContentContainer}>
+        <JoinInput
+          name="oauthEmail"
+          control={control}
+          label="소셜 로그인 아이디"
+          value={oauthEmail}
+          placeholder="이메일 입력"
+          disabled
+          rules={{
+            required: '이메일을 입력해주세요',
+            pattern: {
+              value: /^[^\s@ㄱ-ㅎㅏ-ㅣ가-힣]+@[^\s@ㄱ-ㅎㅏ-ㅣ가-힣]+\.[a-zA-Z]{2,}$/,
+              message: '이메일 형식이 올바르지 않아요',
+            },
+          }}
+        />
+        <JoinInput
+          name="contactEmail"
+          control={control}
+          label="연락 받을 이메일"
+          placeholder="이메일 입력"
+          required
+          rules={{
+            required: '연락 받을 이메일을 입력해주세요',
+            pattern: {
+              value: /^[^\s@ㄱ-ㅎㅏ-ㅣ가-힣]+@[^\s@ㄱ-ㅎㅏ-ㅣ가-힣]+\.[a-zA-Z]{2,}$/,
+              message: '이메일 형식이 올바르지 않아요',
+            },
+          }}
+          tip="로그인 아이디와 달라도 괜찮아요"
+        />
+        <JoinCheckboxContainer
+          serviceAgreeCheck={serviceAgreeCheck}
+          handleAllCheck={handleAllCheck}
+          handleChange={handleChangeCheck}
+        />
+      </div>
+      <button css={nextButton} onClick={handleNext} disabled={!allValid}>
+        다음
+      </button>
+    </section>
+  );
+};
+
+export default JoinEmailStep;
