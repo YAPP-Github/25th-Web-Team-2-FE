7c76559dc8568174cbd0943aa8063b797cd5f384
rbgksqkr <rbgks1937@naver.com>
2025-01-27 02:03:22 +0900

    [YS-224] feat: 참여자 zod schema 선언

---
+++ src/schema/join/ParticipantJoinSchema.ts
@@ -0,0 +1,63 @@
+import { z } from 'zod';
+
+export type ParticipantJoinSchemaType = z.infer<ReturnType<typeof ParticipantJoinSchema>>;
+
+const ParticipantJoinSchema = () => {
+  return z.object({
+    // 소셜 로그인 아이디: 필수. 유저 수정 불가.
+    oauthEmail: z.string().email(),
+
+    // 소셜 로그인 도메인
+    provider: z.union([z.literal('GOOGLE'), z.literal('NAVER')]),
+
+    // 연락받을 이메일: 필수. 한글X. 이메일 형식.
+    contactEmail: z
+      .string()
+      .email({ message: '이메일 형식이 올바르지 않아요' })
+      .nonempty({ message: '연락 받을 이메일을 입력해주세요' }),
+
+    // 이름: 필수. 2자 이상 10자 이하. 한글, 영문만 입력.
+    name: z
+      .string()
+      .regex(/^[a-zA-Z가-힣]+$/, { message: '한글과 영문만 입력 가능합니다' })
+      .min(2, { message: '최소 2자 이상으로 입력해 주세요' })
+      .max(10, { message: '최대 10자 이하로 입력해 주세요' }),
+
+    // 성별. 남자/여자/선택안함
+    gender: z.union([z.literal('MALE'), z.literal('FEMALE'), z.literal('ALL')]),
+
+    // 생년월일. yyyy.mm.dd
+    birthDate: z
+      .string()
+      .regex(/^\d{4}\.\d{2}\.\d{2}$/, { message: 'YYYY.MM.DD 형식으로 입력해주세요' })
+      .refine(
+        (value) => {
+          const [year, month, day] = value.split('.').map(Number);
+          const date = new Date(year, month - 1, day);
+          return (
+            date.getFullYear() === year && date.getMonth() + 1 === month && date.getDate() === day
+          );
+        },
+        { message: '유효한 날짜를 입력해주세요' },
+      ),
+
+    // 거주 지역. 시.도(region), 시.군.구(area)
+    basicAddressInfo: z.object({
+      region: z.string().nonempty({ message: '거주 지역의 시·도를 입력해주세요' }),
+      area: z.string().nonempty({ message: '거주 지역의 시·군·구를 입력해주세요' }),
+    }),
+
+    // 추가 활동 지역. 선택. 시.도(region), 시.군.구(area)
+    additionalAddressInfo: z
+      .object({
+        region: z.string().nonempty({ message: '추가 활동 지역의 시·도를 입력해주세요' }),
+        area: z.string().nonempty({ message: '추가 활동 지역의 시·군·구를 입력해주세요' }),
+      })
+      .optional(),
+
+    // 선호 실험 진행 방식. 선택. 대면/비대면/전체
+    matchType: z.union([z.literal('OFFLINE'), z.literal('ONLINE'), z.literal('ALL')]).optional(),
+  });
+};
+
+export default ParticipantJoinSchema;
