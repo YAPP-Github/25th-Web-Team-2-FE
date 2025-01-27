9cc25a7ba4ec8212e3022c9a80c61057f2ff0870
eeeyooon <vywns9978@gmail.com>
2025-01-27 23:43:13 +0900

    [YS-228] refactor: assignInlineVars를 사용하여 Icon 컴포넌트 style 동적으로 설정

---
+++ src/components/Icon/icon.css.ts
@@ -0,0 +1,43 @@
+import { style } from '@vanilla-extract/css';
+import { recipe } from '@vanilla-extract/recipes';
+
+export const containerStyle = recipe({
+  base: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    transition: 'all 0.3s',
+  },
+  variants: {
+    rotate: {
+      0: { transform: 'rotate(0deg)' },
+      90: { transform: 'rotate(90deg)' },
+      180: { transform: 'rotate(180deg)' },
+      270: { transform: 'rotate(270deg)' },
+    },
+    cursor: {
+      default: { cursor: 'default' },
+      pointer: { cursor: 'pointer' },
+      initial: { cursor: 'initial' },
+      notAllowed: { cursor: 'not-allowed' },
+    },
+  },
+  defaultVariants: {
+    rotate: 0,
+    cursor: 'initial',
+  },
+});
+
+export const dynamicVars = {
+  width: '--icon-width',
+  height: '--icon-height',
+};
+
+export const iconDynamicStyle = style({
+  vars: {
+    [dynamicVars.width]: '24px',
+    [dynamicVars.height]: '24px',
+  },
+  width: `var(${dynamicVars.width})`,
+  height: `var(${dynamicVars.height})`,
+});
