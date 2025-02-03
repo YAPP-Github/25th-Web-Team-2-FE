27bb0d7a4da5799a36878bd8094faada45231a19
eeeyooon <vywns9978@gmail.com>
2025-02-03 23:35:41 +0900

    [YS-238] feat: 이미지 확장자명 외의 파일 첨부 시 토스트 알림

--- src/app/upload/components/DescriptionSection/DescriptionSection.tsx
+++ src/app/upload/components/DescriptionSection/DescriptionSection.tsx
@@ -1,6 +1,6 @@
-/* eslint-disable no-console */
+import * as Toast from '@radix-ui/react-toast';
 import Image from 'next/image';
-import { ChangeEvent, DragEvent } from 'react';
+import { ChangeEvent, DragEvent, useState } from 'react';
 import { Controller, useFormContext } from 'react-hook-form';
 
 import {
@@ -18,6 +18,11 @@ import {
 import InputForm from '../InputForm/InputForm';
 import { headingIcon, uploadSectionLayout } from '../UploadContainer/UploadContainer.css';
 
+import {
+  copyToastLayout,
+  copyToastTitle,
+  copyToastViewport,
+} from '@/app/post/[post_id]/components/ParticipationGuideModal/ParticipationGuideModal.css';
 import Icon from '@/components/Icon';
 import { UploadExperimentPostSchemaType } from '@/schema/upload/uploadExperimentPostSchema';
 import { colors } from '@/styles/colors';
@@ -28,22 +33,33 @@ interface DescriptionSectionProps {
 }
 
 const MAX_PHOTOS = 3;
+const VALID_IMAGE_TYPES = ['image/jpeg', 'image/jpg', 'image/png'];
 
 const DescriptionSection = ({ selectedImages, setSelectedImages }: DescriptionSectionProps) => {
   const { control, formState } = useFormContext<UploadExperimentPostSchemaType>();
   const contentError = formState.errors.content;
+  const [openToast, setOpenToast] = useState(false);
 
   const uploadPhotos = (e: ChangeEvent<HTMLInputElement>): void => {
     const files = e.target.files;
-    if (files) {
-      const newPhotos = Array.from(files);
 
-      if (selectedImages.length + newPhotos.length > MAX_PHOTOS) {
-        const allowedPhotos = newPhotos.slice(0, MAX_PHOTOS - selectedImages.length);
-        setSelectedImages([...selectedImages, ...allowedPhotos]);
-      } else {
-        setSelectedImages([...selectedImages, ...newPhotos]);
+    if (!files) return;
+
+    const newPhotos: File[] = [];
+
+    for (const file of Array.from(files)) {
+      if (!VALID_IMAGE_TYPES.includes(file.type)) {
+        setOpenToast(true);
+        continue;
       }
+      newPhotos.push(file);
+    }
+
+    if (selectedImages.length + newPhotos.length > MAX_PHOTOS) {
+      const allowedPhotos = newPhotos.slice(0, MAX_PHOTOS - selectedImages.length);
+      setSelectedImages([...selectedImages, ...allowedPhotos]);
+    } else {
+      setSelectedImages([...selectedImages, ...newPhotos]);
     }
   };
 
@@ -76,100 +92,118 @@ const DescriptionSection = ({ selectedImages, setSelectedImages }: DescriptionSe
   };
 
   return (
-    <div className={uploadSectionLayout}>
-      {/* 제목 영역 */}
-      <h3 className={uploadFormSectionTitle}>
-        <span className={headingIcon}>2</span>어떤 실험인가요?{' '}
-        <span style={{ color: colors.textAlert }}>*</span>
-      </h3>
+    <>
+      <div className={uploadSectionLayout}>
+        {/* 제목 영역 */}
+        <h3 className={uploadFormSectionTitle}>
+          <span className={headingIcon}>2</span>어떤 실험인가요?{' '}
+          <span style={{ color: colors.textAlert }}>*</span>
+        </h3>
 
-      <div className={descriptionFormLayout}>
-        {/* 실험 제목 */}
-        <Controller
-          name="title"
-          control={control}
-          render={({ field, fieldState }) => (
-            <InputForm
-              {...field}
-              type="text"
-              id="title"
-              placeholder="실험 제목을 입력해 주세요"
-              field={field}
-              fieldState={fieldState}
-              size="full"
-            />
-          )}
-        />
-
-        <div className={descriptionContentContainer({ isError: !!contentError })}>
+        <div className={descriptionFormLayout}>
+          {/* 실험 제목 */}
           <Controller
-            name="content"
+            name="title"
             control={control}
-            render={({ field }) => (
-              <textarea
+            render={({ field, fieldState }) => (
+              <InputForm
                 {...field}
-                id="content"
-                className={descriptionTextarea({
-                  photoGridHeight: selectedImages.length > 0 ? 'withPhotos' : 'withoutPhotos',
-                })}
-                placeholder="본문을 입력해 주세요"
+                type="text"
+                id="title"
+                placeholder="실험 제목을 입력해 주세요"
+                field={field}
+                fieldState={fieldState}
+                size="full"
               />
             )}
           />
 
-          {selectedImages.length > 0 && (
-            <div className={photoGrid}>
-              {selectedImages.map((photo, index) => (
-                <div
-                  className={photoLayout}
-                  key={index}
-                  draggable
-                  onDragStart={(e) => onDragStart(e, index)}
-                  onDragOver={onDragOver}
-                  onDrop={(e) => onDrop(e, index)}
-                >
-                  <div className={photoContainer}>
-                    <button className={deleteButton} onClick={() => deletePhoto(index)}>
-                      <Icon
-                        icon="CloseRound"
-                        width={20}
-                        height={20}
-                        color={colors.field09}
-                        cursor="pointer"
-                        subcolor={colors.field01}
-                      />
-                    </button>
-                    <Image
-                      src={URL.createObjectURL(photo)}
-                      alt="업로드한 이미지 미리보기"
-                      width={80}
-                      height={80}
-                      style={{ objectFit: 'cover', borderRadius: '1.2rem' }}
-                    />
-                  </div>
-                </div>
-              ))}
-            </div>
-          )}
-
-          <div className={uploadImagesContainer}>
-            <label htmlFor="photos" className={addImageContainer}>
-              <Icon icon="ImageAdd" width={16} height={16} />
-              <p style={{ color: colors.text04, fontWeight: '500' }}>사진 추가</p>
-            </label>
-            <input
-              type="file"
-              id="photos"
-              accept="image/*"
-              multiple
-              onChange={uploadPhotos}
-              style={{ display: 'none' }}
+          <div className={descriptionContentContainer({ isError: !!contentError })}>
+            <Controller
+              name="content"
+              control={control}
+              render={({ field }) => (
+                <textarea
+                  {...field}
+                  id="content"
+                  className={descriptionTextarea({
+                    photoGridHeight: selectedImages.length > 0 ? 'withPhotos' : 'withoutPhotos',
+                  })}
+                  placeholder="본문을 입력해 주세요"
+                />
+              )}
             />
-            <p>jpg, png 최대 3장까지 첨부할 수 있어요</p>
+
+            {selectedImages.length > 0 && (
+              <div className={photoGrid}>
+                {selectedImages.map((photo, index) => (
+                  <div
+                    className={photoLayout}
+                    key={index}
+                    draggable
+                    onDragStart={(e) => onDragStart(e, index)}
+                    onDragOver={onDragOver}
+                    onDrop={(e) => onDrop(e, index)}
+                  >
+                    <div className={photoContainer}>
+                      <button className={deleteButton} onClick={() => deletePhoto(index)}>
+                        <Icon
+                          icon="CloseRound"
+                          width={20}
+                          height={20}
+                          color={colors.field09}
+                          cursor="pointer"
+                          subcolor={colors.field01}
+                        />
+                      </button>
+                      <Image
+                        src={URL.createObjectURL(photo)}
+                        alt="업로드한 이미지 미리보기"
+                        width={80}
+                        height={80}
+                        style={{ objectFit: 'cover', borderRadius: '1.2rem' }}
+                      />
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
+
+            <div className={uploadImagesContainer}>
+              <label htmlFor="photos" className={addImageContainer}>
+                <Icon icon="ImageAdd" width={16} height={16} />
+                <p style={{ color: colors.text04, fontWeight: '500' }}>사진 추가</p>
+              </label>
+              <input
+                type="file"
+                id="photos"
+                accept="image/*"
+                multiple
+                onChange={uploadPhotos}
+                style={{ display: 'none' }}
+              />
+              <p>jpg, png 최대 3장까지 첨부할 수 있어요</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+
+      {/* 잘못된 확장자 파일 업로드 시 Toast 알림 */}
+      <Toast.Provider swipeDirection="right">
+        <Toast.Root
+          className={copyToastLayout}
+          open={openToast}
+          onOpenChange={setOpenToast}
+          duration={3500}
+        >
+          <Toast.Title className={copyToastTitle}>
+            <Icon icon="Alert" color={colors.primaryMint} width={24} height={24} />
+            <p>지원되지 않는 파일 형식입니다. JPG 또는 PNG 파일을 업로드하세요.</p>
+          </Toast.Title>
+        </Toast.Root>
+        <Toast.Viewport className={copyToastViewport} />
+      </Toast.Provider>
+    </>
   );
 };
 
