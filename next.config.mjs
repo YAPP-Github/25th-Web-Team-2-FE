4c2fe80d89a2774d5e95ea5546da53e93bb00ac5
eeeyooon <vywns9978@gmail.com>
2025-02-04 00:26:34 +0900

    [YS-238] chore: next.config.mjs에 이미지 외부 도메인 추가

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,9 @@ const nextConfig = {
   compiler: {
     emotion: true,
   },
+  images: {
+    domains: ['dobby-dev-bucket.s3.ap-northeast-2.amazonaws.com'],
+  },
   webpack: (config, { isServer }) => {
     if (isServer) {
       // next server build => ignore msw/browser
