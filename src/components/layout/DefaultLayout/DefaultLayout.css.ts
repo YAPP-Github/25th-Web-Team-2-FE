880f920a92eada3f8b128180b1b7ecceee762661
Gyuhan Park <rbgks1937@naver.com>
2025-02-05 02:18:29 +0900

    [YS-257] design: 최소 높이 유지

--- src/components/layout/DefaultLayout/DefaultLayout.css.ts
+++ src/components/layout/DefaultLayout/DefaultLayout.css.ts
@@ -5,6 +5,7 @@ import { colors } from '@/styles/colors';
 export const defaultLayoutContainer = style({
   backgroundColor: colors.field02,
   paddingBottom: '5.6rem',
+  minHeight: 'calc(100vh - 12.2rem)',
 });
 
 export const defaultLayout = style({
