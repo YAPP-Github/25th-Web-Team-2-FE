5c2fdfcef4c6aa37d1d4714fe9e3e1b2813ff879
eeeyooon <vywns9978@gmail.com>
2025-02-07 00:41:25 +0900

    [YS-253] design: posts 정렬 추가

--- src/app/my-posts/components/MyPostsContainer/MyPostsContainer.tsx
+++ src/app/my-posts/components/MyPostsContainer/MyPostsContainer.tsx
@@ -1,12 +1,26 @@
 'use client';
 
-import { myPostContainerLayout, myPostsHeader } from './MyPostsContainer.css';
+import {
+  myPostContainerLayout,
+  myPostsHeaderContainer,
+  myPostsHeaderText,
+  postsSorting,
+} from './MyPostsContainer.css';
 import MyPostsTable from '../MyPostsTable/MyPostsTable';
+import Icon from '@/components/Icon';
+import { colors } from '@/styles/colors';
 
 const MyPostsContainer = () => {
   return (
     <div className={myPostContainerLayout}>
-      <h2 className={myPostsHeader}>내가 작성한 글</h2>
+      <div className={myPostsHeaderContainer}>
+        <h2 className={myPostsHeaderText}>내가 작성한 글</h2>
+        <div className={postsSorting}>
+          <p>최신순</p>
+          <Icon icon="ArrowSorting" width={20} height={20} color={colors.icon04} />
+        </div>
+      </div>
+
       <MyPostsTable />
     </div>
   );
