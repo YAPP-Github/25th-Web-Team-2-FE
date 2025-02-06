7e6d44182bbb3bc84b31ca1cfd43f01f6b97638c
Gyuhan Park <rbgks1937@naver.com>
2025-02-07 05:06:32 +0900

    [YS-257] refactor: Filter 컴포넌트들을 FilterContainer 하위 디렉토리로 이동

--- src/app/home/components/PostContainer/FilterContainer/FilterContainer.tsx
+++ src/app/home/components/PostContainer/FilterContainer/FilterContainer.tsx
@@ -1,8 +1,8 @@
+import AreaFilter from './AreaFilter/AreaFilter';
+import { verticalLine } from './AreaFilter/AreaFilter.css';
+import ContactTargetFilter from './ContactTargetPopover/ContactTargetFilter';
 import { filterContainerLayout, resetFilterButton } from './FilterContainer.css';
-import AreaFilter from '../AreaFilter/AreaFilter';
-import { verticalLine } from '../AreaFilter/AreaFilter.css';
-import ContactTargetFilter from '../ContactTargetPopover/ContactTargetFilter';
-import MatchTypeFilter from '../MatchTypeFilter/MatchTypeFilter';
+import MatchTypeFilter from './MatchTypeFilter/MatchTypeFilter';
 
 import { ExperimentPostListFilters } from '@/apis/post';
 import Icon from '@/components/Icon';
