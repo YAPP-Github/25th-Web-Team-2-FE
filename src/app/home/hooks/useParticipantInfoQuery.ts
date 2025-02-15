a2b489db1ebe01d1abd324ff1b5b2c9730f105db
Gyuhan Park <rbgks1937@naver.com>
2025-02-15 16:11:34 +0900

    [YS-301] refactor: 유저 정보 쿼리키 추가

--- src/app/home/hooks/useParticipantInfoQuery.ts
+++ src/app/home/hooks/useParticipantInfoQuery.ts
@@ -1,6 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 
 import { getParticipantInfo } from '@/apis/login';
+import { QUERY_KEY } from '@/constants/queryKey';
 
 interface useParticipantInfoQueryProps {
   enabled: boolean;
@@ -8,7 +9,7 @@ interface useParticipantInfoQueryProps {
 
 const useParticipantInfoQuery = ({ enabled }: useParticipantInfoQueryProps) => {
   return useQuery({
-    queryKey: ['participantInfo'],
+    queryKey: [QUERY_KEY.participantInfo],
     queryFn: getParticipantInfo,
     enabled,
     retry: 1,
--- src/constants/queryKey.ts
+++ src/constants/queryKey.ts
@@ -5,4 +5,6 @@ export const QUERY_KEY = {
   updateRecruitStatus: 'updateRecruitStatus',
   deletePost: 'deletePost',
   experimentPostDetail: 'experimentPostDetail',
+  participantInfo: 'participantInfo',
+  researcherInfo: 'researcherInfo',
 };
