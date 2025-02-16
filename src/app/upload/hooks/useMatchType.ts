1aa64c8fbcf622b0887d18ef74ad5c8e0465ab6d
eeeyooon <vywns9978@gmail.com>
2025-02-16 16:50:52 +0900

    [YS-290] refactor: 기존 typeGuard 사용

--- src/app/upload/hooks/useMatchType.ts
+++ src/app/upload/hooks/useMatchType.ts
@@ -1,9 +1,9 @@
 import { usePathname } from 'next/navigation';
 import { useFormContext, useWatch } from 'react-hook-form';
 
-import { ParticipantResponse, ResearcherResponse } from '@/apis/login';
 import useUserInfo from '@/app/home/hooks/useUserInfo';
 import { MatchType } from '@/types/uploadExperimentPost';
+import { isResearcherInfo } from '@/utils/typeGuard';
 
 const useMatchType = () => {
   const pathname = usePathname();
@@ -12,10 +12,6 @@ const useMatchType = () => {
   const { userInfo } = useUserInfo();
 
   const selectedMatchType = useWatch({ control, name: 'matchType' });
-
-  const isResearcher = (
-    user: ParticipantResponse | ResearcherResponse,
-  ): user is ResearcherResponse => (user as ResearcherResponse).memberInfo.role === 'RESEARCHER';
 
   // 대면 방식 선택 (비대면이면 실험 장소 null)
   const handleMatchTypeChange = (value: MatchType | null) => {
@@ -29,7 +25,7 @@ const useMatchType = () => {
     } else {
       setValue('region', '');
       setValue('area', '');
-      setValue('place', !isEdit && userInfo && isResearcher(userInfo) ? userInfo.univName : '');
+      setValue('place', !isEdit && userInfo && isResearcherInfo(userInfo) ? userInfo.univName : '');
       setValue('detailedAddress', '');
     }
   };
