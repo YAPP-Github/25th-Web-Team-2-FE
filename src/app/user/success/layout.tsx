0d33bd7260bc946a0294047653037e18037b4293
Gyuhan Park <rbgks1937@naver.com>
2025-02-20 01:57:42 +0900

    [YS-334] design: 탈퇴 성공 페이지 구현

---
+++ src/app/user/success/layout.tsx
@@ -0,0 +1,12 @@
+import { Suspense } from 'react';
+import { leaveSuccessPageLayout } from './success.css';
+
+function LeaveSuccessLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className={leaveSuccessPageLayout}>
+      <Suspense>{children}</Suspense>
+    </div>
+  );
+}
+
+export default LeaveSuccessLayout;
---
+++ src/app/user/success/page.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import {
+  homeLink,
+  image,
+  leaveSuccessLayout,
+  leaveTitleContainer,
+  subTitle,
+  title,
+} from './success.css';
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useEffect } from 'react';
+import JoinSuccess from '@/assets/images/joinSuccess.svg';
+import Logo from '@/assets/images/logo.svg';
+import useSessionStorage from '@/hooks/useSessionStorage';
+
+const LeaveSuccessPage = () => {
+  const { clear } = useSessionStorage();
+
+  useEffect(() => {
+    clear();
+  }, []);
+
+  return (
+    <div className={leaveSuccessLayout}>
+      <div className={leaveTitleContainer}>
+        <div>
+          <Link href="/">
+            <Image src={Logo} alt="로고" className={image} />
+          </Link>
+          <h2 className={title}>회원 탈퇴가 완료되었어요</h2>
+          <h3 className={subTitle}>그라밋이 필요할 때 언제든 다시 찾아주세요</h3>
+        </div>
+        <Image src={JoinSuccess} alt="회원가입 성공" width={160} height={140} className={image} />
+      </div>
+      <Link href="/" className={homeLink}>
+        홈 화면으로 돌아가기
+      </Link>
+    </div>
+  );
+};
+
+export default LeaveSuccessPage;
