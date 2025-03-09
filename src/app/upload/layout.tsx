09adb09e5e42024f56062bb6377321fb63bc78a8
eeeyooon <vywns9978@gmail.com>
2025-03-09 15:57:31 +0900

    [YS-384] feat: 페이지별 Metadata 추가

--- src/app/upload/layout.tsx
+++ src/app/upload/layout.tsx
@@ -1,4 +1,11 @@
+import { Metadata } from 'next';
+
 import DefaultLayout from '@/components/layout/DefaultLayout/DefaultLayout';
+
+export const metadata: Metadata = {
+  title: '그라밋 | 실험 공고 등록',
+  description: '그라밋 | 실험 공고 등록',
+};
 
 function UploadLayout({ children }: { children: React.ReactNode }) {
   return <DefaultLayout>{children}</DefaultLayout>;
--- src/app/user/profile/layout.tsx
+++ src/app/user/profile/layout.tsx
@@ -1,6 +1,13 @@
+import { Metadata } from 'next';
+
 import { profilePageLayout } from './ProfilePage.css';
 
 import UserLayout from '@/components/layout/UserLayout/UserLayout';
+
+export const metadata: Metadata = {
+  title: '그라밋 | 회원 정보',
+  description: '그라밋 | 회원 정보',
+};
 
 function ProfileLayout({ children }: { children: React.ReactNode }) {
   return (
