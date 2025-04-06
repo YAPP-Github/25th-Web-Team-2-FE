31211689067f59972dfcf3e976c2ec76d274dc7e
rbgksqkr <rbgks1937@naver.com>
2025-04-06 19:26:51 +0900

    [YS-207] refactor: 유저 정보 확인 후 우측 상단 UI 렌더링

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,16 +3,12 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-import { buttonContainer, contactButton, headerLayout, image, loginButton } from './Header.css';
-import Menu from './Menu';
+import { headerLayout, image } from './Header.css';
 import Logo from '../../assets/images/logo.svg';
 
-import useUserInfo from '@/app/home/hooks/useUserInfo';
-import { isResearcherInfo } from '@/utils/typeGuard';
+import RightHeader from './RightHeader/RightHeader';
 
 const Header = () => {
-  const { userInfo } = useUserInfo();
-
   return (
     <div className={headerLayout}>
       <Link href="/">
@@ -26,22 +22,7 @@ const Header = () => {
           style={{ width: 'auto', height: 'auto' }}
         />
       </Link>
-      <div className={buttonContainer}>
-        {userInfo ? (
-          <>
-            {isResearcherInfo(userInfo) && (
-              <Link href="/upload">
-                <button className={contactButton}>실험 공고 등록</button>
-              </Link>
-            )}
-            <Menu userInfo={userInfo} />
-          </>
-        ) : (
-          <Link href="/login" className={loginButton}>
-            로그인
-          </Link>
-        )}
-      </div>
+      <RightHeader />
     </div>
   );
 };
