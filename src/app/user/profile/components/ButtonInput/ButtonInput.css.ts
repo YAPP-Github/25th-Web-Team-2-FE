a5805d4bdf28936dbdcecf524f3dea4d3ef2c6db
rbgksqkr <rbgks1937@naver.com>
2025-04-13 23:22:06 +0900

    [YS-413] [YS-409] design: 헬퍼텍스트와 에러 메시지 한줄로 노출

---
+++ src/app/user/profile/components/ButtonInput/ButtonInput.css.ts
@@ -0,0 +1,64 @@
+import { style } from '@vanilla-extract/css';
+
+import { colors } from '@/styles/colors';
+import { fonts } from '@/styles/fonts.css';
+
+export const inputContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '0.6rem',
+  width: '100%',
+});
+
+export const inputLabel = style({
+  ...fonts.label.large.M14,
+  color: colors.text06,
+  display: 'flex',
+  gap: '0.4rem',
+});
+
+export const joinInput = style({
+  ...fonts.body.normal.M16,
+  color: colors.text06,
+  border: `0.1rem solid ${colors.line01}`,
+  padding: '1.6rem',
+  borderRadius: '1.2rem',
+  selectors: {
+    '&:disabled': {
+      color: colors.text03,
+      backgroundColor: colors.field03,
+    },
+    '&::placeholder': {
+      color: colors.text03,
+    },
+    '&:focus': {
+      outline: `0.1rem solid ${colors.lineTinted}`,
+    },
+    "&[aria-invalid='true']": {
+      outline: `0.1rem solid ${colors.textAlert}`,
+    },
+  },
+});
+
+export const requiredStar = style({
+  color: colors.textAlert,
+});
+
+export const infoContainer = style({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  width: '100%',
+});
+
+export const errorMessage = style({
+  ...fonts.label.medium.R13,
+  color: colors.textAlert,
+});
+
+export const tipWrapper = style({
+  ...fonts.label.medium.R13,
+  display: 'flex',
+  gap: '0.4rem',
+  color: colors.text02,
+});
--- src/app/user/profile/components/ButtonInput/ButtonInput.tsx
+++ src/app/user/profile/components/ButtonInput/ButtonInput.tsx
@@ -2,13 +2,14 @@ import React, { useRef, useState } from 'react';
 import { Control, Controller, FieldValues, Path } from 'react-hook-form';
 
 import {
-  errorMessage,
   inputContainer,
   inputLabel,
+  errorMessage,
   joinInput,
   requiredStar,
   tipWrapper,
-} from '@/app/join/components/JoinInput/JoinInput.css';
+  infoContainer,
+} from './ButtonInput.css';
 import {
   univAuthButton,
   univInputWrapper,
@@ -87,12 +88,17 @@ const ButtonInput = <T extends FieldValues>({
                   </button>
                 )}
               </div>
-              {fieldState.error && <span className={errorMessage}>{fieldState.error.message}</span>}
-              {tip && (
-                <div className={tipWrapper}>
-                  <span>{tip}</span>
-                </div>
-              )}
+              <div className={infoContainer}>
+                {fieldState.error ? (
+                  <span className={errorMessage}>{fieldState.error.message}</span>
+                ) : (
+                  tip && (
+                    <div className={tipWrapper}>
+                      <span>{tip}</span>
+                    </div>
+                  )
+                )}
+              </div>
             </>
           );
         }}
