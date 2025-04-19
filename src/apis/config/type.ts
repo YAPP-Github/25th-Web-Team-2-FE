7a5c4eebd7c43c6b6e6d8c329a2dfd139f62fea5
rbgksqkr <rbgks1937@naver.com>
2025-04-19 18:00:32 +0900

    [YS-409] feat: config 파일만 분리하여 복잡한 인터셉터 로직 분리

--- src/apis/config/type.ts
+++ src/apis/config/type.ts
@@ -1,8 +1,13 @@
 import { AxiosError } from 'axios';
+
 import { ERROR_MESSAGES } from './constants';
 
+export type ErrorCode = keyof typeof ERROR_MESSAGES;
+
+export type AuthErrorCode = 'AU0001' | 'AU0002' | 'AU0003' | 'AU0004' | 'AU0005';
+
 interface ApiErrorResponse {
-  code: keyof typeof ERROR_MESSAGES;
+  code: ErrorCode;
   message: string;
   data: unknown;
 }
---
+++ src/apis/config/utils.ts
@@ -0,0 +1,51 @@
+import axios, { InternalAxiosRequestConfig } from 'axios';
+import { AuthErrorCode } from './type';
+import { getSession } from 'next-auth/react';
+import { updateAccessToken } from '../login';
+import { ERROR_MESSAGES } from './constants';
+import { loginWithCredentials, logout } from '@/lib/auth-utils';
+import { API } from '.';
+
+export const isAuthError = (code: string) => {
+  return (
+    code === 'AU0001' ||
+    code === 'AU0002' ||
+    code === 'AU0003' ||
+    code === 'AU0004' ||
+    code === 'AU0005'
+  );
+};
+
+export const login = async ({
+  request,
+  code,
+}: {
+  request: InternalAxiosRequestConfig;
+  code: AuthErrorCode;
+}) => {
+  try {
+    // Next-Auth 세션에서 리프레시 토큰 가져오기
+    const session = await getSession();
+    const refreshToken = session?.refreshToken;
+
+    if (!refreshToken) {
+      return Promise.reject({ message: ERROR_MESSAGES[code] });
+    }
+
+    const userInfo = await updateAccessToken(refreshToken);
+
+    // Next-Auth 재로그인
+    await loginWithCredentials({
+      accessToken: userInfo.accessToken,
+      refreshToken: userInfo.refreshToken,
+      role: userInfo.memberInfo.role,
+    });
+
+    request.headers.Authorization = `Bearer ${userInfo.accessToken}`;
+    API.defaults.headers.Authorization = `Bearer ${userInfo.accessToken}`;
+    return axios(request);
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (err) {
+    await logout();
+  }
+};
