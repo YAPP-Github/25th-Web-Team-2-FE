cd8ffac07a15f0e73a506f703a8a2d43dd304400
eeeyooon <vywns9978@gmail.com>
2025-04-23 15:29:58 +0900

    [YS-420] design: 배경색 gradient 수정

--- src/components/layout/DefaultLayout/DefaultLayout.css.ts
+++ src/components/layout/DefaultLayout/DefaultLayout.css.ts
@@ -3,10 +3,11 @@ import { style } from '@vanilla-extract/css';
 import { colors } from '@/styles/colors';
 
 export const defaultLayoutContainer = style({
-  background: `linear-gradient(to bottom, ${colors.field01} 0%, ${colors.field01} 8%, ${colors.field02} 13%, ${colors.field02} 100%)`,
-
+  position: 'relative',
   paddingBottom: '5.6rem',
   minHeight: 'calc(100vh - 12.2rem)',
+  overflow: 'hidden',
+  background: `linear-gradient(to bottom, ${colors.field01} 0%, ${colors.field01} 6%, ${colors.field02} 12%, ${colors.field02} 100%)`,
 });
 
 export const defaultLayout = style({
