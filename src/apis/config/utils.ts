bfc7d8d934204fe671250d50a9abee8181bfd251
rbgksqkr <rbgks1937@naver.com>
2025-05-05 17:41:11 +0900

    [YS-430] chore: axios 로직 제거

--- src/apis/config/utils.ts
+++ src/apis/config/utils.ts
@@ -1,13 +1,6 @@
-import axios, { AxiosInstance, InternalAxiosRequestConfig } from 'axios';
 import { getServerSession } from 'next-auth';
 import { getSession } from 'next-auth/react';
-
-import { AuthErrorCode } from './types';
-import { updateAccessToken } from '../login';
-import { ERROR_MESSAGES } from './constants';
-import { CustomError } from './error';
-
-import { authOptions, loginWithCredentials } from '@/lib/auth-utils';
+import { authOptions } from '@/lib/auth-utils';
 
 export const isAuthError = (code: string) => {
   return (
@@ -17,43 +10,6 @@ export const isAuthError = (code: string) => {
     code === 'AU0004' ||
     code === 'AU0005'
   );
-};
-
-export const login = async ({
-  axiosInstance,
-  request,
-  code,
-  status,
-}: {
-  axiosInstance: AxiosInstance;
-  request: InternalAxiosRequestConfig;
-  code: AuthErrorCode;
-  status: number;
-}) => {
-  // Next-Auth 세션에서 리프레시 토큰 가져오기
-  const session = await getSession();
-  const refreshToken = session?.refreshToken;
-
-  if (!refreshToken) {
-    throw new CustomError({
-      status,
-      code,
-      message: ERROR_MESSAGES[code],
-    });
-  }
-
-  const userInfo = await updateAccessToken(refreshToken);
-
-  // Next-Auth 재로그인
-  await loginWithCredentials({
-    accessToken: userInfo.accessToken,
-    refreshToken: userInfo.refreshToken,
-    role: userInfo.memberInfo.role,
-  });
-
-  request.headers.Authorization = `Bearer ${userInfo.accessToken}`;
-  axiosInstance.defaults.headers.Authorization = `Bearer ${userInfo.accessToken}`;
-  return axios(request);
 };
 
 export const getSessionRefreshToken = async () => {
