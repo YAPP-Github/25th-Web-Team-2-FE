c14e41d16e7dc93860d7536c202c4fd4d66709c2
rbgksqkr <rbgks1937@naver.com>
2025-05-10 15:55:27 +0900

    [YS-430] design: DefaultLayout overflow:hidden 제거

--- src/components/layout/DefaultLayout/DefaultLayout.css.ts
+++ src/components/layout/DefaultLayout/DefaultLayout.css.ts
@@ -6,7 +6,6 @@ export const defaultLayoutContainer = style({
   position: 'relative',
   paddingBottom: '5.6rem',
   minHeight: 'calc(100vh - 12.2rem)',
-  overflow: 'hidden',
   background: `linear-gradient(to bottom, ${colors.field01} 0%, ${colors.field01} 6%, ${colors.field02} 12%, ${colors.field02} 100%)`,
 });
 
