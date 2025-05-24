3790e509c5644fc49af35be3c5beac9b05621169
rbgksqkr <rbgks1937@naver.com>
2025-05-24 14:30:58 +0900

    [YS-434] fix: Layout이 중복으로 적용되는 문제 해결

--- src/app/join/layout.tsx
+++ src/app/join/layout.tsx
@@ -1,9 +1,5 @@
 import { Metadata } from 'next';
-import { headers } from 'next/headers';
 import { Suspense } from 'react';
-
-import DesktopJoinLayout from './desktop/layout';
-import MobileJoinLayout from './mobile/layout';
 
 export const metadata: Metadata = {
   title: '그라밋 | 회원가입',
@@ -11,17 +7,5 @@ export const metadata: Metadata = {
 };
 
 export default function JoinLayout({ children }: { children: React.ReactNode }) {
-  const headersList = headers();
-  const userAgent = headersList.get('user-agent') || '';
-  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
-
-  return isMobile ? (
-    <MobileJoinLayout>
-      <Suspense>{children}</Suspense>
-    </MobileJoinLayout>
-  ) : (
-    <DesktopJoinLayout>
-      <Suspense>{children}</Suspense>
-    </DesktopJoinLayout>
-  );
+  return <Suspense>{children}</Suspense>;
 }
