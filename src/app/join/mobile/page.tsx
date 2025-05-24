61d52f3c654b088326e5cd797f69d7c30f9934a4
rbgksqkr <rbgks1937@naver.com>
2025-05-24 23:20:37 +0900

    [YS-434] refactor: components 디렉토리로 이동

--- src/app/join/mobile/page.tsx
+++ src/app/join/mobile/page.tsx
@@ -2,7 +2,7 @@
 
 import { useSession } from 'next-auth/react';
 
-import JoinHeader from './JoinHeader';
+import JoinHeader from './components/JoinHeader/JoinHeader';
 import ParticipantForm from './ParticipantForm';
 import ResearcherForm from './ResearcherForm';
 import useFunnel from '../hooks/useFunnel';
