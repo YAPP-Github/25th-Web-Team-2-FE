bbd8313a2b568c81d0e0f5d91ecf92ba87ca9724
rbgksqkr <rbgks1937@naver.com>
2025-05-25 16:56:44 +0900

    [YS-434] refactor: 로그인 페이지 desktop 디렉토리로 분리

--- src/app/login/desktop/layout.tsx
+++ src/app/login/desktop/layout.tsx
@@ -1,14 +1,12 @@
 import { Metadata } from 'next';
 
-import { loginLayout } from './LoginPage.css';
+import { loginLayout } from '../LoginPage.css';
 
 export const metadata: Metadata = {
   title: '그라밋 | 로그인',
   description: '그라밋 | 로그인',
 };
 
-function LoginLayout({ children }: { children: React.ReactNode }) {
+export default function DesktopLoginLayout({ children }: { children: React.ReactNode }) {
   return <div className={loginLayout}>{children}</div>;
 }
-
-export default LoginLayout;
