b61a7448be712ceaa65be9410f10ea1a3cec54b7
rbgksqkr <rbgks1937@naver.com>
2025-06-03 00:51:46 +0900

    [YS-450] refactor: MatchTypeFilter 중복값 상수 또는 함수로 분리

--- src/app/home/components/ExperimentPostContainer/FilterContainer/MatchTypeFilter/MatchTypeFilter.tsx
+++ src/app/home/components/ExperimentPostContainer/FilterContainer/MatchTypeFilter/MatchTypeFilter.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import * as Select from '@radix-ui/react-select';
 import { assignInlineVars } from '@vanilla-extract/dynamic';
 import { useState } from 'react';
@@ -7,10 +5,10 @@ import { useState } from 'react';
 import { triggerWrapper, contentContainer, selectItem } from './MatchTypeFilter.css';
 
 import { ExperimentPostListFilters } from '@/apis/post';
-import { getFilterColors } from '@/app/home/home.utils';
+import { getFilterColors, getMatchTypeLabel } from '@/app/home/home.utils';
 import Icon from '@/components/Icon';
+import { MATCH_TYPE_OPTIONS } from '@/app/home/home.constants';
 
-const matchTypeMapper = { ALL: '전체', OFFLINE: '대면', ONLINE: '비대면' };
 interface MatchTypeFilterProps {
   filters: ExperimentPostListFilters;
   onChange: (value: string) => void;
@@ -21,21 +19,17 @@ const MatchTypeFilter = ({ filters, onChange }: MatchTypeFilterProps) => {
 
   const isSelected = Boolean(filters.matchType);
 
-  const handleValueChange = (value: string) => {
-    onChange(value);
-  };
-
   return (
     <Select.Root
       value={filters.matchType ? filters.matchType : ''}
-      onValueChange={handleValueChange}
+      onValueChange={onChange}
       onOpenChange={(open) => setIsOpen(open)}
     >
       <Select.Trigger
         className={triggerWrapper}
         style={assignInlineVars(getFilterColors(isSelected))}
       >
-        <span>{filters.matchType ? matchTypeMapper[filters.matchType] : '진행 방식'}</span>
+        <span>{getMatchTypeLabel(filters.matchType)}</span>
         <Select.Icon>
           <Icon icon="Chevron" width={20} rotate={isOpen ? -180 : 0} cursor="pointer" />
         </Select.Icon>
@@ -44,15 +38,11 @@ const MatchTypeFilter = ({ filters, onChange }: MatchTypeFilterProps) => {
         <Select.Content className={contentContainer} position="popper" sideOffset={6} align="start">
           <Select.Viewport>
             <Select.Group>
-              <Select.Item value="ALL" className={selectItem}>
-                <Select.ItemText>전체</Select.ItemText>
-              </Select.Item>
-              <Select.Item value="OFFLINE" className={selectItem}>
-                <Select.ItemText>대면</Select.ItemText>
-              </Select.Item>
-              <Select.Item value="ONLINE" className={selectItem}>
-                <Select.ItemText>비대면</Select.ItemText>
-              </Select.Item>
+              {MATCH_TYPE_OPTIONS.map((option) => (
+                <Select.Item key={option.value} value={option.value} className={selectItem}>
+                  <Select.ItemText>{option.label}</Select.ItemText>
+                </Select.Item>
+              ))}
             </Select.Group>
           </Select.Viewport>
         </Select.Content>
--- src/app/home/home.constants.ts
+++ src/app/home/home.constants.ts
@@ -342,3 +342,9 @@ export const GENDER = [
   { label: '남성', value: 'MALE' },
   { label: '여성', value: 'FEMALE' },
 ] as const;
+
+export const MATCH_TYPE_OPTIONS = [
+  { label: '전체', value: 'ALL' },
+  { label: '대면', value: 'OFFLINE' },
+  { label: '비대면', value: 'ONLINE' },
+] as const;
