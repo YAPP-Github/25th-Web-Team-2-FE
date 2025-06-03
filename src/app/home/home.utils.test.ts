139884dc499e0c4005b1fce8fc7b64f5291506ef
rbgksqkr <rbgks1937@naver.com>
2025-06-04 00:46:34 +0900

    [YS-455] test: 만나이 계산 유틸 함수 테스트 코드 구현

--- src/app/home/home.utils.test.ts
+++ src/app/home/home.utils.test.ts
@@ -1,7 +1,7 @@
-import { describe, expect, it } from 'vitest';
-import { formatPostDate } from './home.utils';
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
+import { calculateAgeFromBirthDate, formatPostDate } from './home.utils';
 
-describe('formatPostDate 유틸 함수 테스트', () => {
+describe('formatPostDate - 실험공고 날짜 형식 맞추는 유틸 함수 테스트', () => {
   it('시작일과 종료일이 모두 있는 경우 범위 형식으로 날짜 문자열을 반환한다.', () => {
     // Given
     const startDate = '2025-01-15';
@@ -49,6 +49,40 @@ describe('formatPostDate 유틸 함수 테스트', () => {
 
     // When
     const result = formatPostDate({ startDate, endDate });
+
+    // Then
+    expect(result).toBe(expected);
+  });
+});
+
+describe('calculateAgeFromBirthDate - 만 나이 계산 유틸 함수 테스트', () => {
+  beforeEach(() => {
+    vi.setSystemTime(new Date('2025-01-15'));
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
+  });
+
+  it('생일이 지나지 않은 경우의 만 나이를 계산하여 반환한다.', () => {
+    // Given
+    const birthDate = '2000-01-16';
+    const expected = 24;
+
+    // When
+    const result = calculateAgeFromBirthDate(birthDate);
+
+    // Then
+    expect(result).toBe(expected);
+  });
+
+  it('생일이 지난 경우의 만 나이를 계산하여 반환한다.', () => {
+    // Given
+    const birthDate = '2000-01-14';
+    const expected = 25;
+
+    // When
+    const result = calculateAgeFromBirthDate(birthDate);
 
     // Then
     expect(result).toBe(expected);
