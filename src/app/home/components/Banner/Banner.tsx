cfe5241c11aa19c4850c2e77257ae5b64cc99b8a
rbgksqkr <rbgks1937@naver.com>
2025-06-03 19:04:00 +0900

    [YS-450] refactor: BannerMap 상수로 배너 이미지 데이터 관리

--- src/app/home/components/Banner/Banner.tsx
+++ src/app/home/components/Banner/Banner.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import Image from 'next/image';
-import { useRef } from 'react';
 
 import {
   bannerCarousel,
@@ -19,15 +18,34 @@ import { useSlide } from './hooks/useSlide';
 import { useTouchSlide } from './hooks/useTouchSlide';
 import { SLIDE_SPEED } from '../../home.constants';
 
+const BannerMap = [
+  {
+    src: WebBanner,
+    alt: '참여자 언제 다 모을지 고민이라면 공고를 올리고 가까운 참여자에게 실험을 알려보세요',
+  },
+  {
+    src: WebBannerSecond,
+    alt: '공강 시간에 부담 없이 용돈 버는 방법 학교 근처 실험에 참여하고 보상을 받아보세요',
+  },
+  {
+    src: WebBanner,
+    alt: '참여자 언제 다 모을지 고민이라면 공고를 올리고 가까운 참여자에게 실험을 알려보세요',
+  },
+  {
+    src: WebBannerSecond,
+    alt: '공강 시간에 부담 없이 용돈 버는 방법 학교 근처 실험에 참여하고 보상을 받아보세요',
+  },
+];
+
 const Banner = () => {
-  const { bannerIdx, resetAutoSlide, moveSlide, handleClickPrev, handleClickNext } = useSlide();
+  const { bannerIdx, carouselRef, resetAutoSlide, moveSlide, handleClickPrev, handleClickNext } =
+    useSlide();
+
   const { handleTouchStart, handleTouchMove, handleTouchEnd } = useTouchSlide({
     bannerIdx,
     resetAutoSlide,
     moveSlide,
   });
-
-  const carouselRef = useRef<HTMLDivElement>(null);
 
   return (
     <div className={bannerLayout}>
@@ -44,42 +62,18 @@ const Banner = () => {
             transition: `transform ${SLIDE_SPEED}s ease-in-out`,
           }}
         >
-          <Image
-            src={WebBanner}
-            alt="참여자 언제 다 모을지 고민이라면 공고를 올리고 가까운 참여자에게 실험을 알려보세요"
-            className={bannerImage}
-            priority
-            width={1000}
-            height={80}
-            style={{ width: 'auto', height: 'auto' }}
-          />
-          <Image
-            src={WebBannerSecond}
-            alt="공강 시간에 부담 없이 용돈 버는 방법 학교 근처 실험에 참여하고 보상을 받아보세요"
-            className={bannerImage}
-            priority
-            width={1000}
-            height={80}
-            style={{ width: 'auto', height: 'auto' }}
-          />
-          <Image
-            src={WebBanner}
-            alt="참여자 언제 다 모을지 고민이라면 공고를 올리고 가까운 참여자에게 실험을 알려보세요"
-            className={bannerImage}
-            priority
-            width={1000}
-            height={80}
-            style={{ width: 'auto', height: 'auto' }}
-          />
-          <Image
-            src={WebBannerSecond}
-            alt="공강 시간에 부담 없이 용돈 버는 방법 학교 근처 실험에 참여하고 보상을 받아보세요"
-            className={bannerImage}
-            priority
-            width={1000}
-            height={80}
-            style={{ width: 'auto', height: 'auto' }}
-          />
+          {BannerMap.map((banner, idx) => (
+            <Image
+              key={idx}
+              src={banner.src}
+              alt={banner.alt}
+              className={bannerImage}
+              priority
+              width={1000}
+              height={80}
+              style={{ width: 'auto', height: 'auto' }}
+            />
+          ))}
         </div>
       </div>
 
