86259c99d00dce7ef94e4971c3303b0da925cb03
rbgksqkr <rbgks1937@naver.com>
2025-06-06 17:25:30 +0900

    refactor: 좀더 직관적으로 noRetryFetchClient로 네이밍 수정

--- src/apis/config/fetchClient.ts
+++ src/apis/config/fetchClient.ts
@@ -66,4 +66,4 @@ export const createSSRFetchClient = (accessToken?: string) => {
  * retryLogin 없는 fetchClient
  * @description 토큰 재발급 시 무한 루프 방지를 위해 retryLogin 없는 fetchClient 생성
  */
-export const refreshClient = createBaseFetchClient();
+export const noRetryFetchClient = createBaseFetchClient();
