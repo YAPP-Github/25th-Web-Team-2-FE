cd603857ea3b590774a2ab53bdb5fc367dfc315c
eeeyooon <vywns9978@gmail.com>
2025-06-10 16:56:49 +0900

    [YS-447] design: 참여 방법 바텀 시트 디자인 적용

--- src/app/post/[post_id]/mobile/components/ParticipationGuideBottomSheet/ParticipationGuideBottomSheet.css.ts
+++ src/app/post/[post_id]/mobile/components/ParticipationGuideBottomSheet/ParticipationGuideBottomSheet.css.ts
@@ -2,18 +2,93 @@ import { style } from '@vanilla-extract/css';
 
 import { colors } from '@/styles/colors';
 import { fonts } from '@/styles/fonts.css';
+import { zIndex } from '@/styles/zIndex';
 
 export const participationGuideBottomSheetLayout = style({
   minWidth: '3.7rem',
-  margin: '0 1.5rem',
-  backgroundColor: colors.primaryTinted,
-
-  zIndex: 999,
 });
 
 export const bottomSheetTitle = style({
   ...fonts.title.small.SB18,
   color: colors.text06,
 
+  marginBottom: '1.2rem',
+});
+
+export const contactInfoRowContainer = style({
+  display: 'flex',
+  flexDirection: 'row',
+  gap: '2.2rem',
+  marginBottom: '0.8rem',
+  ...fonts.body.normal.M16,
+});
+
+export const contactInfoTitle = style({
+  ...fonts.label.large.M14,
+  width: '3.7rem',
+  color: colors.text03,
+});
+
+export const contactInfoContent = style({
+  ...fonts.label.large.M14,
+  color: colors.text06,
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  gap: '0.6rem',
+  maxWidth: '27.7rem',
+  whiteSpace: 'normal',
+  wordBreak: 'break-all',
+});
+
+export const applyMethodContainer = style({
+  display: 'flex',
+  flexFlow: 'column nowrap',
+  justifyContent: 'center',
+
   marginBottom: '1.6rem',
+});
+
+export const warningMessage = style({
+  width: 'fit-content',
+  marginTop: '1.6rem',
+  padding: '0.4rem 1.2rem',
+  borderRadius: '1.2rem',
+  backgroundColor: colors.fieldAlert,
+  color: colors.textAlert,
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  gap: '0.6rem',
+  ...fonts.label.small.M12,
+});
+
+export const copyToastLayout = style({
+  height: '5.2rem',
+  backgroundColor: colors.fieldToast,
+  color: colors.text01,
+  borderRadius: '8rem',
+  padding: '1.4rem 2.4rem',
+  position: 'fixed',
+  top: '0',
+  left: '50%',
+  transform: 'translateX(-50%)',
+  zIndex: zIndex.toastContent,
+});
+
+export const copyToastTitle = style({
+  ...fonts.body.normal.SB16,
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  gap: '1rem',
+  width: 'max-content',
+});
+
+export const copyToastViewport = style({
+  position: 'fixed',
+  top: '6rem',
+  left: '50%',
+  transform: 'translateX(-50%)',
+  zIndex: zIndex.toastViewport,
 });
--- src/app/post/[post_id]/mobile/components/ParticipationGuideBottomSheet/ParticipationGuideBottomSheet.tsx
+++ src/app/post/[post_id]/mobile/components/ParticipationGuideBottomSheet/ParticipationGuideBottomSheet.tsx
@@ -1,10 +1,24 @@
+import * as Toast from '@radix-ui/react-toast';
+import { useState } from 'react';
+
 import {
   participationGuideBottomSheetLayout,
   bottomSheetTitle,
+  applyMethodContainer,
+  contactInfoContent,
+  contactInfoRowContainer,
+  contactInfoTitle,
+  warningMessage,
+  copyToastLayout,
+  copyToastTitle,
+  copyToastViewport,
 } from './ParticipationGuideBottomSheet.css';
 import useApplyMethodQuery from '../../../hooks/useApplyMethodQuery';
 
 import Button from '@/components/Button/Button';
+import Icon from '@/components/Icon';
+import { trackEvent } from '@/lib/mixpanelClient';
+import { colors } from '@/styles/colors';
 
 const ParticipationGuideBottomSheet = ({
   onConfirm,
@@ -13,6 +27,8 @@ const ParticipationGuideBottomSheet = ({
   onConfirm: VoidFunction;
   postId: string;
 }) => {
+  const [isCopyToastOpen, setIsCopyToastOpen] = useState(false);
+
   /* 공고 지원 방법 조회 */
   const { data: applyMethodData, isLoading: isLoadingApply } = useApplyMethodQuery({ postId });
 
@@ -22,16 +38,114 @@ const ParticipationGuideBottomSheet = ({
   }
   if (!applyMethodData) return <div>데이터 없음</div>;
 
+  const handleCopyContent = (text: string) => {
+    navigator.clipboard.writeText(text).then(() => {
+      setIsCopyToastOpen(true);
+      trackEvent('ApplyMethod Interaction', {
+        action: 'Link Copied',
+      });
+    });
+  };
+
   return (
     <section className={participationGuideBottomSheetLayout}>
       <h2 className={bottomSheetTitle}>참여 방법</h2>
-      <p>{applyMethodData.content}</p>
 
-      <div>
-        <Button onClick={onConfirm} variant="dark" size="small" height={'5.6rem'}>
-          확인
-        </Button>
+      <div
+        className={applyMethodContainer}
+        style={{
+          alignItems: applyMethodData.formUrl || applyMethodData.phoneNum ? 'flex-start' : 'center',
+        }}
+      >
+        <h3
+          className={bottomSheetTitle}
+          style={{
+            textAlign: applyMethodData.formUrl || applyMethodData.phoneNum ? 'left' : 'center',
+          }}
+        >
+          {applyMethodData.content}
+        </h3>
+
+        {/* 링크 */}
+        {applyMethodData.formUrl && (
+          <div className={contactInfoRowContainer}>
+            <span className={contactInfoTitle}>링크</span>
+            <div className={contactInfoContent}>
+              <a
+                href={applyMethodData.formUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ color: colors.primaryMint, textDecoration: 'underline' }}
+                onClick={() => {
+                  trackEvent('ApplyMethod Interaction', {
+                    action: 'Link Clicked',
+                    link_url: applyMethodData.formUrl ?? '',
+                  });
+                }}
+              >
+                {applyMethodData.formUrl}
+              </a>
+              <Icon
+                icon="Copy"
+                width={20}
+                height={20}
+                cursor="pointer"
+                color={colors.text04}
+                onClick={() =>
+                  applyMethodData.formUrl && handleCopyContent(applyMethodData.formUrl)
+                }
+              />
+            </div>
+          </div>
+        )}
+
+        {/* 연락처 */}
+        {applyMethodData.phoneNum && (
+          <div className={contactInfoRowContainer}>
+            <span className={contactInfoTitle}>연락처</span>
+            <div className={contactInfoContent}>
+              {applyMethodData.phoneNum}
+              <Icon
+                icon="Copy"
+                width={20}
+                height={20}
+                cursor="pointer"
+                color={colors.text04}
+                onClick={() =>
+                  applyMethodData.phoneNum && handleCopyContent(applyMethodData.phoneNum)
+                }
+              />
+            </div>
+          </div>
+        )}
+
+        {/* 개인정보보호 안내 */}
+        <div className={warningMessage}>
+          <Icon icon="AlertOutlined" color={colors.textAlert} width={18} height={18} />
+          개인정보보호에 유의해주세요
+        </div>
       </div>
+
+      <Button onClick={onConfirm} variant="dark" size="small" height={'5.6rem'}>
+        확인
+      </Button>
+
+      {/* todo Toast 위치 변경 예정  */}
+      {/* 복사 성공 토스트 알림 */}
+      <Toast.Provider swipeDirection="right">
+        <Toast.Root
+          className={copyToastLayout}
+          open={isCopyToastOpen}
+          onOpenChange={setIsCopyToastOpen}
+          duration={1500}
+        >
+          <Toast.Title className={copyToastTitle}>
+            <Icon icon="CheckRound" color={colors.primaryMint} width={24} height={24} />
+            <p>복사되었어요</p>
+          </Toast.Title>
+        </Toast.Root>
+        <Toast.Viewport className={copyToastViewport} />
+      </Toast.Provider>
     </section>
   );
 };
