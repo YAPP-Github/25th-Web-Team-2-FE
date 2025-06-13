c9bc7753918b69bb8ac77fdd58d66461b900a8cd
rbgksqkr <rbgks1937@naver.com>
2025-06-13 10:32:32 +0900

    [YS-462] feat: 연구자 모바일 회원가입 학교 메일 UI 구현

--- src/app/join/mobile/Researcher/UnivEmailStep/UnivEmailStep.tsx
+++ src/app/join/mobile/Researcher/UnivEmailStep/UnivEmailStep.tsx
@@ -1,9 +1,137 @@
+import { useState } from 'react';
+import { Controller, useFormContext, useWatch } from 'react-hook-form';
+
+import TitleSection from '../../components/TitleSection/TitleSection';
+import { mainContentLayout } from '../../page.css';
+
+import EmailToast from '@/app/join/components/EmailToast/EmailToast';
+import { errorMessage, joinInput } from '@/app/join/components/JoinInput/JoinInput.css';
+import AuthCodeInput from '@/app/join/desktop/Researcher/JoinEmailStep/UnivAuthInput/AuthCodeInput/AuthCodeInput';
+import {
+  editButton,
+  univAuthButton,
+  univInputWrapper,
+} from '@/app/join/desktop/Researcher/JoinEmailStep/UnivAuthInput/UnivAuthInput.css';
+import useAuthCodeTimer from '@/app/join/hooks/useAuthCodeTimer';
+import useSendUnivAuthCodeMutation from '@/app/join/hooks/useSendUnivAuthCodeMutation';
+import { inputContainer } from '@/components/ButtonInput/ButtonInput.css';
+import { ResearcherJoinSchemaType } from '@/schema/join/ResearcherJoinSchema';
+
+const getButtonText = (isLoading: boolean, isAuthenticated: boolean) => {
+  if (isLoading) return '전송 중...';
+  if (isAuthenticated) return '수정';
+  return '인증번호 전송';
+};
+
 interface UnivEmailStepProps {
   onNext: () => void;
 }
 
 const UnivEmailStep = ({ onNext }: UnivEmailStepProps) => {
-  return <div>UnivEmailStep</div>;
+  const { control, getValues, setValue, clearErrors, trigger } =
+    useFormContext<ResearcherJoinSchemaType>();
+
+  const {
+    data: authCodeData,
+    mutate: sendEmail,
+    error: authCodeError,
+    isPending: isLoadingSend,
+  } = useSendUnivAuthCodeMutation();
+
+  const { authTimer, startTimer, stopTimer } = useAuthCodeTimer();
+
+  const [isEmailSent, setIsEmailSent] = useState(false);
+  const [isToastOpen, setIsToastOpen] = useState(false);
+
+  const isEmailVerified = useWatch({ name: 'isEmailVerified', control });
+  const isUnivEmailAuthenticated = isEmailSent || isEmailVerified;
+
+  const handleSendUnivAuthCode = () => {
+    sendEmail(getValues('univEmail'), {
+      onSuccess: () => {
+        setIsEmailSent(true);
+        setIsToastOpen(true);
+        startTimer();
+      },
+      onError: (error) => {
+        if (error.code === 'VE0007') {
+          setValue('isEmailVerified', true);
+        }
+      },
+    });
+  };
+
+  const handleClickEdit = () => {
+    setIsEmailSent(false);
+    setValue('isEmailVerified', false);
+    stopTimer();
+    clearErrors('univEmail');
+  };
+
+  return (
+    <main className={mainContentLayout}>
+      <TitleSection
+        title="학교 메일을 입력해 주세요"
+        description={`대학원생임을 인증하기 위해 필요해요\n추후 수정할 수 없으니 신중히 입력해 주세요`}
+      />
+
+      <Controller
+        name="univEmail"
+        control={control}
+        render={({ field, fieldState }) => {
+          const isButtonDisabled =
+            (!isEmailSent && !field.value) || isLoadingSend || fieldState.invalid;
+
+          const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+            field.onChange(e);
+            await trigger('univEmail');
+          };
+
+          return (
+            <div className={inputContainer}>
+              <div className={univInputWrapper}>
+                <input
+                  {...field}
+                  style={{ width: '100%' }}
+                  className={joinInput}
+                  placeholder="학교 메일 입력"
+                  aria-invalid={fieldState.invalid ? true : false}
+                  disabled={isUnivEmailAuthenticated}
+                  onChange={handleChange}
+                />
+                <button
+                  type="button"
+                  className={`${univAuthButton} ${isUnivEmailAuthenticated ? editButton : ''}`}
+                  disabled={isButtonDisabled}
+                  onClick={isUnivEmailAuthenticated ? handleClickEdit : handleSendUnivAuthCode}
+                >
+                  {getButtonText(isLoadingSend, isUnivEmailAuthenticated)}
+                </button>
+              </div>
+              {fieldState.error ? (
+                <span className={errorMessage}>{fieldState.error.message}</span>
+              ) : (
+                <span className={errorMessage}>{authCodeError && authCodeError.message}</span>
+              )}
+            </div>
+          );
+        }}
+      />
+
+      {isEmailSent && (
+        <AuthCodeInput
+          authTimer={authTimer}
+          handleSendUnivAuthCode={handleSendUnivAuthCode}
+          stopTimer={stopTimer}
+        />
+      )}
+      <EmailToast
+        title={`인증번호가 발송되었어요. (${authCodeData?.requestCount}회 / 하루 최대 3회)`}
+        isToastOpen={isToastOpen}
+        setIsToastOpen={setIsToastOpen}
+      />
+    </main>
+  );
 };
 
 export default UnivEmailStep;
