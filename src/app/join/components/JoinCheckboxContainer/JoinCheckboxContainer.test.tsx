9f3afb14e737facbf735578b04b268843f92e78a
rbgksqkr <rbgks1937@naver.com>
2025-06-16 02:03:39 +0900

    [YS-486] refactor: 중복으로 선언된 버튼 찾는 코드 분리

--- src/app/join/components/JoinCheckboxContainer/JoinCheckboxContainer.test.tsx
+++ src/app/join/components/JoinCheckboxContainer/JoinCheckboxContainer.test.tsx
@@ -1,7 +1,7 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { describe, expect, it } from 'vitest';
+import { beforeEach, describe, expect, it } from 'vitest';
 
 import JoinCheckboxContainer from './JoinCheckboxContainer';
 
@@ -9,196 +9,101 @@ import { ParticipantJoinSchema } from '@/schema/join/ParticipantJoinSchema';
 import { renderWithForm } from '@/tests/test-utils';
 
 describe('JoinCheckboxContainer', () => {
+  let allCheckbox: HTMLElement;
+  let termsCheckbox: HTMLElement;
+  let privacyCheckbox: HTMLElement;
+  let adConsentCheckbox: HTMLElement;
+  let matchConsentCheckbox: HTMLElement;
+
+  beforeEach(() => {
+    renderWithForm(<JoinCheckboxContainer />, {
+      formProps: {
+        resolver: zodResolver(ParticipantJoinSchema()),
+        defaultValues: {
+          isTermOfService: false,
+          isPrivacy: false,
+          adConsent: false,
+          matchConsent: false,
+        },
+      },
+    });
+
+    allCheckbox = screen.getByRole('checkbox', { name: /이용약관에 모두 동의합니다$/ });
+    termsCheckbox = screen.getByRole('checkbox', { name: /서비스 이용약관 동의$/ });
+    privacyCheckbox = screen.getByRole('checkbox', { name: /개인정보 수집 및 이용 동의$/ });
+    adConsentCheckbox = screen.getByRole('checkbox', {
+      name: /\[선택\] 광고성 정보 이메일\/SMS 수신 동의$/,
+    });
+    matchConsentCheckbox = screen.getByRole('checkbox', {
+      name: /\[선택\] 개인정보 수집 및 이용 동의-실험 추천·혜택$/,
+    });
+  });
+
   it('초기엔 모든 체크박스가 비활성화되어 있다.', async () => {
-    // given
-    renderWithForm(<JoinCheckboxContainer />, {
-      formProps: {
-        resolver: zodResolver(ParticipantJoinSchema()),
-        defaultValues: {
-          isTermOfService: false,
-          isPrivacy: false,
-          adConsent: false,
-        },
-      },
-    });
-
-    // when
-    const allCheckbox = screen.getByRole('checkbox', { name: /이용약관에 모두 동의합니다$/ });
-    const termsCheckbox = screen.getByRole('checkbox', { name: /서비스 이용약관 동의$/ });
-    const privacyCheckbox = screen.getByRole('checkbox', { name: /개인정보 수집 및 이용 동의$/ });
-    const adConsentCheckbox = screen.getByRole('checkbox', {
-      name: /\[선택\] 광고성 정보 이메일\/SMS 수신 동의$/,
-    });
-
     // then
     expect(allCheckbox).not.toBeChecked();
     expect(termsCheckbox).not.toBeChecked();
     expect(privacyCheckbox).not.toBeChecked();
     expect(adConsentCheckbox).not.toBeChecked();
+    expect(matchConsentCheckbox).not.toBeChecked();
   });
 
-  it('모두 동의 체크박스를 클릭하면 모든 체크박스가 체크된다.', async () => {
-    // given
-    const user = userEvent.setup();
-    renderWithForm(<JoinCheckboxContainer />, {
-      formProps: {
-        resolver: zodResolver(ParticipantJoinSchema()),
-        defaultValues: {
-          isTermOfService: false,
-          isPrivacy: false,
-          adConsent: false,
-        },
-      },
-    });
-    const allCheckbox = screen.getByRole('checkbox', { name: /이용약관에 모두 동의합니다$/ });
-    const termsCheckbox = screen.getByRole('checkbox', { name: /서비스 이용약관 동의$/ });
-    const privacyCheckbox = screen.getByRole('checkbox', { name: /개인정보 수집 및 이용 동의$/ });
-    const adConsentCheckbox = screen.getByRole('checkbox', {
-      name: /\[선택\] 광고성 정보 이메일\/SMS 수신 동의$/,
+  describe('"모두 동의" 체크박스를 클릭했을 때', () => {
+    it('모든 개별 체크박스가 선택된다.', async () => {
+      // given
+      const user = userEvent.setup();
+
+      // when
+      await user.click(allCheckbox);
+
+      // then
+      expect(termsCheckbox).toBeChecked();
+      expect(privacyCheckbox).toBeChecked();
+      expect(adConsentCheckbox).toBeChecked();
+      expect(matchConsentCheckbox).toBeChecked();
     });
 
-    // when
-    await user.click(allCheckbox);
+    it('다시 클릭하면 모든 개별 체크박스가 해제된다.', async () => {
+      // given
+      const user = userEvent.setup();
 
-    // then
-    expect(allCheckbox).toBeChecked();
-    expect(termsCheckbox).toBeChecked();
-    expect(privacyCheckbox).toBeChecked();
-    expect(adConsentCheckbox).toBeChecked();
+      // when
+      await user.click(allCheckbox); // 전체 선택
+      await user.click(allCheckbox); // 전체 해제
+
+      // then
+      expect(termsCheckbox).not.toBeChecked();
+      expect(privacyCheckbox).not.toBeChecked();
+      expect(adConsentCheckbox).not.toBeChecked();
+      expect(matchConsentCheckbox).not.toBeChecked();
+    });
   });
+  describe('개별 체크박스를 클릭했을 때', () => {
+    it('모든 개별 체크박스가 선택되면, "모두 동의" 체크박스도 선택된다.', async () => {
+      // given
+      const user = userEvent.setup();
 
-  it('이미 하나가 체크된 상태에서 모두 동의 체크박스를 체크하면 모든 체크박스가 체크된다.', async () => {
-    // given
-    const user = userEvent.setup();
-    renderWithForm(<JoinCheckboxContainer />, {
-      formProps: {
-        resolver: zodResolver(ParticipantJoinSchema()),
-        defaultValues: {
-          isTermOfService: false,
-          isPrivacy: false,
-          adConsent: false,
-        },
-      },
-    });
-    const allCheckbox = screen.getByRole('checkbox', { name: /이용약관에 모두 동의합니다$/ });
-    const termsCheckbox = screen.getByRole('checkbox', { name: /서비스 이용약관 동의$/ });
-    const privacyCheckbox = screen.getByRole('checkbox', { name: /개인정보 수집 및 이용 동의$/ });
-    const adConsentCheckbox = screen.getByRole('checkbox', {
-      name: /\[선택\] 광고성 정보 이메일\/SMS 수신 동의$/,
-    });
-    await user.click(termsCheckbox);
+      // when
+      await user.click(termsCheckbox);
+      await user.click(privacyCheckbox);
+      await user.click(adConsentCheckbox);
+      await user.click(matchConsentCheckbox);
 
-    // when
-    await user.click(allCheckbox);
-
-    // then
-    expect(allCheckbox).toBeChecked();
-    expect(termsCheckbox).toBeChecked();
-    expect(privacyCheckbox).toBeChecked();
-    expect(adConsentCheckbox).toBeChecked();
-  });
-
-  it('이미 하나가 체크된 상태에서 모두 동의 체크박스를 체크 해제하면 모든 체크박스가 체크 해제된다.', async () => {
-    // given
-    const user = userEvent.setup();
-    renderWithForm(<JoinCheckboxContainer />, {
-      formProps: {
-        resolver: zodResolver(ParticipantJoinSchema()),
-        defaultValues: {
-          isTermOfService: false,
-          isPrivacy: false,
-          adConsent: false,
-        },
-      },
-    });
-    const allCheckbox = screen.getByRole('checkbox', { name: /이용약관에 모두 동의합니다$/ });
-    const termsCheckbox = screen.getByRole('checkbox', { name: /서비스 이용약관 동의$/ });
-    const privacyCheckbox = screen.getByRole('checkbox', { name: /개인정보 수집 및 이용 동의$/ });
-    const adConsentCheckbox = screen.getByRole('checkbox', {
-      name: /\[선택\] 광고성 정보 이메일\/SMS 수신 동의$/,
-    });
-    await user.click(termsCheckbox);
-
-    // when
-    await user.click(allCheckbox);
-    await user.click(allCheckbox);
-
-    // then
-    expect(allCheckbox).not.toBeChecked();
-    expect(termsCheckbox).not.toBeChecked();
-    expect(privacyCheckbox).not.toBeChecked();
-    expect(adConsentCheckbox).not.toBeChecked();
-  });
-
-  it('나머지 체크박스를 모두 체크하면 모두 동의 체크박스가 체크된다.', async () => {
-    // given
-    const user = userEvent.setup();
-    renderWithForm(<JoinCheckboxContainer />, {
-      formProps: {
-        resolver: zodResolver(ParticipantJoinSchema()),
-        defaultValues: {
-          isTermOfService: false,
-          isPrivacy: false,
-          adConsent: false,
-          matchConsent: false,
-        },
-      },
-    });
-    const allCheckbox = screen.getByRole('checkbox', { name: /이용약관에 모두 동의합니다$/ });
-    const termsCheckbox = screen.getByRole('checkbox', { name: /서비스 이용약관 동의$/ });
-    const privacyCheckbox = screen.getByRole('checkbox', { name: /개인정보 수집 및 이용 동의$/ });
-    const adConsentCheckbox = screen.getByRole('checkbox', {
-      name: /\[선택\] 광고성 정보 이메일\/SMS 수신 동의$/,
-    });
-    const matchConsentCheckbox = screen.getByRole('checkbox', {
-      name: /\[선택\] 개인정보 수집 및 이용 동의-실험 추천·혜택$/,
+      // then
+      expect(allCheckbox).toBeChecked();
     });
 
-    // when
-    await user.click(termsCheckbox);
-    await user.click(privacyCheckbox);
-    await user.click(adConsentCheckbox);
-    await user.click(matchConsentCheckbox);
+    it('전체가 선택된 상태에서 개별 체크박스 하나를 해제하면, "모두 동의" 체크박스도 해제된다.', async () => {
+      // given
+      const user = userEvent.setup();
+      await user.click(allCheckbox);
+      expect(allCheckbox).toBeChecked();
 
-    // then
-    expect(allCheckbox).toBeChecked();
-  });
+      // when
+      await user.click(termsCheckbox);
 
-  it('모두 체크된 상태에서 하나의 체크박스를 체크 해제하면 "모두 동의 체크 박스"가 체크 해제된다.', async () => {
-    // given & when
-    const user = userEvent.setup();
-    renderWithForm(<JoinCheckboxContainer />, {
-      formProps: {
-        resolver: zodResolver(ParticipantJoinSchema()),
-        defaultValues: {
-          isTermOfService: false,
-          isPrivacy: false,
-          adConsent: false,
-          matchConsent: false,
-        },
-      },
+      // then
+      expect(allCheckbox).not.toBeChecked();
     });
-    const allCheckbox = screen.getByRole('checkbox', { name: /이용약관에 모두 동의합니다$/ });
-    const termsCheckbox = screen.getByRole('checkbox', { name: /서비스 이용약관 동의$/ });
-    const privacyCheckbox = screen.getByRole('checkbox', { name: /개인정보 수집 및 이용 동의$/ });
-    const adConsentCheckbox = screen.getByRole('checkbox', {
-      name: /\[선택\] 광고성 정보 이메일\/SMS 수신 동의$/,
-    });
-    const matchConsentCheckbox = screen.getByRole('checkbox', {
-      name: /\[선택\] 개인정보 수집 및 이용 동의-실험 추천·혜택$/,
-    });
-    await user.click(termsCheckbox);
-    await user.click(privacyCheckbox);
-    await user.click(adConsentCheckbox);
-    await user.click(matchConsentCheckbox);
-
-    // then
-    expect(allCheckbox).toBeChecked();
-
-    // when
-    await user.click(matchConsentCheckbox);
-
-    // then
-    expect(allCheckbox).not.toBeChecked();
   });
 });
