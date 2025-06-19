9d2fd32641c1e7cb82f1e81e648b08483f1c5dca
rbgksqkr <rbgks1937@naver.com>
2025-06-19 22:52:34 +0900

    [YS-486] refactor: 테스트 케이스 명시적으로 분리

--- src/app/join/desktop/Researcher/JoinEmailStep/NextButton/NextButton.test.tsx
+++ src/app/join/desktop/Researcher/JoinEmailStep/NextButton/NextButton.test.tsx
@@ -8,6 +8,16 @@ import NextButton from './NextButton';
 import { ResearcherJoinSchema } from '@/schema/join/ResearcherJoinSchema';
 import { renderWithForm } from '@/tests/test-utils';
 
+const validValues = {
+  oauthEmail: 'oauth@email.com',
+  contactEmail: 'contact@email.com',
+  univEmail: 'univEmaill@cau.ac.kr',
+  verifiedContactEmail: 'contact@email.com',
+  isEmailVerified: true,
+  isTermOfService: true,
+  isPrivacy: true,
+};
+
 describe('NextButton (Researcher - desktop)', () => {
   it('초기 상태는 버튼이 비활성화되어 있다.', () => {
     // given
@@ -19,20 +29,11 @@ describe('NextButton (Researcher - desktop)', () => {
     expect(nextButton).toBeDisabled();
   });
 
-  it('모든 필수 입력값이 존재하면 다음 버튼이 활성화되고, 다음 버튼을 클릭했을 때 유효하면 onNext 함수를 호출한다.', async () => {
-    // given
-    const user = userEvent.setup();
-    const onNext = vi.fn();
+  // “입력값이 유효하지않으면 onNext 함수를 호출하지 않는다.”
 
-    const validValues = {
-      oauthEmail: 'oauth@email.com',
-      contactEmail: 'contact@email.com',
-      univEmail: 'univEmaill@cau.ac.kr',
-      verifiedContactEmail: 'contact@email.com',
-      isEmailVerified: true,
-      isTermOfService: true,
-      isPrivacy: true,
-    };
+  it('모든 필수 입력값이 존재하면 다음 버튼이 활성화된다.', () => {
+    // given
+    const onNext = vi.fn();
 
     renderWithForm(<NextButton onNext={onNext} />, {
       formProps: {
@@ -46,48 +47,45 @@ describe('NextButton (Researcher - desktop)', () => {
 
     // then
     expect(nextButton).toBeEnabled();
+  });
+
+  it('입력값이 유효하면 onNext 함수를 호출한다.', async () => {
+    // given
+    const user = userEvent.setup();
+    const onNext = vi.fn();
+
+    renderWithForm(<NextButton onNext={onNext} />, {
+      formProps: {
+        resolver: zodResolver(ResearcherJoinSchema()),
+        defaultValues: validValues,
+      },
+    });
 
     // when
+    const nextButton = screen.getByRole('button', { name: '다음' });
     await user.click(nextButton);
 
     // then
     expect(onNext).toHaveBeenCalledTimes(1);
   });
 
-  it('모든 필수 입력값이 존재하면 다음 버튼이 활성화되지만, 입력값이 유효하지 않으면 클릭했을 때 onNext 함수를 호출하지 않는다.', async () => {
+  it('입력값이 유효하지 않으면 버튼을 클릭해도 onNext 함수가 호출되지 않는다.', async () => {
     // given
     const user = userEvent.setup();
     const onNext = vi.fn();
 
-    const validValues = {
-      oauthEmail: 'oauth@email.com',
-      contactEmail: 'contact@email.com',
-      univEmail: 'univEmaill@cau.ac.kr',
-      verifiedContactEmail: 'contact@email.com',
-      isEmailVerified: true,
-      isTermOfService: true,
-      isPrivacy: true,
-    };
-
-    const invalidUnivEmail = 'invalidUnivEmail';
-
     renderWithForm(<NextButton onNext={onNext} />, {
       formProps: {
         resolver: zodResolver(ResearcherJoinSchema()),
-        defaultValues: { ...validValues, univEmail: invalidUnivEmail },
       },
     });
 
     // when
     const nextButton = screen.getByRole('button', { name: '다음' });
-
-    // then
-    expect(nextButton).toBeEnabled();
-
-    // when
     await user.click(nextButton);
 
     // then
-    expect(onNext).not.toHaveBeenCalledTimes(1);
+    expect(nextButton).toBeDisabled();
+    expect(onNext).toHaveBeenCalledTimes(0);
   });
 });
