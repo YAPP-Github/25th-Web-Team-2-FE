50cff9fb0c0238266fa834a9cb3290ffa58e1091
rbgksqkr <rbgks1937@naver.com>
2025-06-19 23:49:12 +0900

    [YS-480] feat: 텍스트 맵핑 객체 활용

--- src/app/post/[postId]/mobile/components/EditNotReadyModal/EditNotReadyModal.tsx
+++ src/app/post/[postId]/mobile/components/EditNotReadyModal/EditNotReadyModal.tsx
@@ -20,7 +20,7 @@ import NotReadyMobile from '@/assets/images/notReadyMobile.svg';
 import Icon from '@/components/Icon';
 import { setHideModalCookie } from '@/lib/cookies';
 
-export type NotReadyMenu = 'profile' | 'upload' | 'edit';
+export type NotReadyMenu = 'profile' | 'upload' | 'edit' | 'myPosts';
 export interface NotReadyModalProps {
   isOpen: boolean;
   onOpenChange: (open: boolean) => void;
