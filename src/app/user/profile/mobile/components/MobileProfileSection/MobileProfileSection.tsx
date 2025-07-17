c78af6b7f2c1fcc46b1f37ba9bda412510ccdb67
rbgksqkr <rbgks1937@naver.com>
2025-07-17 19:16:54 +0900

    [YS-513] refactor: 추가 거주 지역도 텍스트로 UI에 노출

--- src/app/user/profile/mobile/components/MobileProfileSection/MobileProfileSection.tsx
+++ src/app/user/profile/mobile/components/MobileProfileSection/MobileProfileSection.tsx
@@ -1,14 +1,15 @@
 import ProfileItem from '../ProfileItem/ProfileItem';
 
-import Icon from '@/components/Icon';
 import { profileSectionLayout } from './MobileProfileSection.css';
 import useUserInfo from '@/app/home/hooks/useUserInfo';
 import { isParticipantInfo } from '@/utils/typeGuard';
-import { ParticipantResponse, ResearcherResponse } from '@/apis/login';
+import { ParticipantResponse } from '@/apis/login';
 import { useSession } from 'next-auth/react';
 import { AREA_MAPPER, REGION_MAPPER } from '@/app/home/home.constants';
+import MatchConsentToggle from '../MatchConsentToggle/MatchConsentToggle';
+import AdConsentToggle from '../AdConsentToggle/AdConsentToggle';
 
-const PROFILE_FIELDS_MAP = {
+const MOBILE_PROFILE_FIELDS_MAP = {
   PARTICIPANT: [
     {
       required: true,
@@ -23,12 +24,20 @@ const PROFILE_FIELDS_MAP = {
     {
       required: true,
       title: '거주 지역',
-      getLabel: (userInfo: ParticipantResponse) =>
-        userInfo.basicAddressInfo
-          ? `${REGION_MAPPER[userInfo.basicAddressInfo.region]} / ${
-              AREA_MAPPER[userInfo.basicAddressInfo.area]
-            }`
-          : '',
+      getLabel: (userInfo: ParticipantResponse) => {
+        const region = REGION_MAPPER[userInfo.basicAddressInfo.region];
+        const area = AREA_MAPPER[userInfo.basicAddressInfo.area];
+        const isAdditionalAddress = userInfo.additionalAddressInfo.region !== 'NONE';
+
+        if (isAdditionalAddress) {
+          const additionalRegion = REGION_MAPPER[userInfo.additionalAddressInfo.region];
+          const additionalArea = AREA_MAPPER[userInfo.additionalAddressInfo.area];
+
+          return `${region} ${area} / ${additionalRegion} ${additionalArea}`;
+        }
+
+        return `${region} ${area}`;
+      },
       isIcon: true,
     },
     {
@@ -37,12 +46,12 @@ const PROFILE_FIELDS_MAP = {
     },
     {
       title: '광고성 정보 이메일/SMS 수신 동의',
-      rightElement: <Icon icon="Airplane" />,
+      rightElement: (userInfo: ParticipantResponse) => <AdConsentToggle userInfo={userInfo} />,
       isArrow: false,
     },
     {
       title: '개인정보 수집 및 이용 동의-실험 추천·혜택',
-      rightElement: <Icon icon="Airplane" />,
+      rightElement: (userInfo: ParticipantResponse) => <MatchConsentToggle userInfo={userInfo} />,
       isArrow: false,
     },
   ],
@@ -54,27 +63,29 @@ const MobileProfileSection = () => {
   const { data } = useSession();
   const role = data?.role;
 
-  const fields = role ? PROFILE_FIELDS_MAP[role] : [];
+  const fields = role ? MOBILE_PROFILE_FIELDS_MAP[role] : [];
 
-  if (isParticipantInfo(userInfo)) {
-    return (
-      <section className={profileSectionLayout}>
-        {fields.map((field) => (
-          <ProfileItem
-            key={field.title}
-            required={field.required}
-            title={field.title}
-            label={typeof field.getLabel === 'function' ? field.getLabel(userInfo) : ''}
-            isIcon={field.isIcon}
-            isArrow={field.isArrow}
-            rightElement={field.rightElement}
-          />
-        ))}
-      </section>
-    );
-  }
+  if (!isParticipantInfo(userInfo)) return null;
 
-  return null;
+  return (
+    <section className={profileSectionLayout}>
+      {fields.map((field) => (
+        <ProfileItem
+          key={field.title}
+          required={field.required}
+          title={field.title}
+          label={typeof field.getLabel === 'function' ? field.getLabel(userInfo) : ''}
+          isIcon={field.isIcon}
+          isArrow={field.isArrow}
+          rightElement={
+            typeof field.rightElement === 'function'
+              ? field.rightElement(userInfo)
+              : field.rightElement
+          }
+        />
+      ))}
+    </section>
+  );
 };
 
 export default MobileProfileSection;
