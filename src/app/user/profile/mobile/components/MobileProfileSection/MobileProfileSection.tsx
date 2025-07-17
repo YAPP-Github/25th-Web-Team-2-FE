7023c13998fba4c8a791a96e9591c30ff6cff244
rbgksqkr <rbgks1937@naver.com>
2025-07-17 18:34:25 +0900

    [YS-513] feat: MobileProfileSection 구현

--- src/app/user/profile/mobile/components/MobileProfileSection/MobileProfileSection.tsx
+++ src/app/user/profile/mobile/components/MobileProfileSection/MobileProfileSection.tsx
@@ -1,31 +1,80 @@
 import ProfileItem from '../ProfileItem/ProfileItem';
 
 import Icon from '@/components/Icon';
+import { profileSectionLayout } from './MobileProfileSection.css';
+import useUserInfo from '@/app/home/hooks/useUserInfo';
+import { isParticipantInfo } from '@/utils/typeGuard';
+import { ParticipantResponse, ResearcherResponse } from '@/apis/login';
+import { useSession } from 'next-auth/react';
+import { AREA_MAPPER, REGION_MAPPER } from '@/app/home/home.constants';
+
+const PROFILE_FIELDS_MAP = {
+  PARTICIPANT: [
+    {
+      required: true,
+      title: '연락 받을 이메일',
+      getLabel: (userInfo: ParticipantResponse) => userInfo.memberInfo.contactEmail,
+    },
+    {
+      required: true,
+      title: '이름',
+      getLabel: (userInfo: ParticipantResponse) => userInfo.memberInfo.name,
+    },
+    {
+      required: true,
+      title: '거주 지역',
+      getLabel: (userInfo: ParticipantResponse) =>
+        userInfo.basicAddressInfo
+          ? `${REGION_MAPPER[userInfo.basicAddressInfo.region]} / ${
+              AREA_MAPPER[userInfo.basicAddressInfo.area]
+            }`
+          : '',
+      isIcon: true,
+    },
+    {
+      title: '선호 실험 진행 방식',
+      getLabel: (userInfo: ParticipantResponse) => userInfo.matchType ?? '-',
+    },
+    {
+      title: '광고성 정보 이메일/SMS 수신 동의',
+      rightElement: <Icon icon="Airplane" />,
+      isArrow: false,
+    },
+    {
+      title: '개인정보 수집 및 이용 동의-실험 추천·혜택',
+      rightElement: <Icon icon="Airplane" />,
+      isArrow: false,
+    },
+  ],
+  RESEARCHER: [],
+};
 
 const MobileProfileSection = () => {
-  return (
-    <div style={{ padding: '1.6rem' }}>
-      <ProfileItem required title="연락 받을 이메일" label="Yap@gmail.com" />
-      <ProfileItem required title="이름" label="김참여" />
-      <ProfileItem
-        required
-        isIcon
-        title="거주 지역"
-        label="서울특별시 성동구 / 서울특별시 광진구"
-      />
-      <ProfileItem title="선호 실험 진행 방식" label="전체" />
-      <ProfileItem
-        title="광고성 정보 이메일/SMS 수신 동의"
-        rightElement={<Icon icon="Airplane" />}
-        isArrow={false}
-      />
-      <ProfileItem
-        title="개인정보 수집 및 이용 동의-실험 추천·혜택"
-        rightElement={<Icon icon="Airplane" />}
-        isArrow={false}
-      />
-    </div>
-  );
+  const { userInfo } = useUserInfo();
+  const { data } = useSession();
+  const role = data?.role;
+
+  const fields = role ? PROFILE_FIELDS_MAP[role] : [];
+
+  if (isParticipantInfo(userInfo)) {
+    return (
+      <section className={profileSectionLayout}>
+        {fields.map((field) => (
+          <ProfileItem
+            key={field.title}
+            required={field.required}
+            title={field.title}
+            label={typeof field.getLabel === 'function' ? field.getLabel(userInfo) : ''}
+            isIcon={field.isIcon}
+            isArrow={field.isArrow}
+            rightElement={field.rightElement}
+          />
+        ))}
+      </section>
+    );
+  }
+
+  return null;
 };
 
 export default MobileProfileSection;
