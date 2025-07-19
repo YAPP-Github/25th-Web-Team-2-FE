8d0534b74847fe75b8a3a2d3ab9347ca019ca0b0
rbgksqkr <rbgks1937@naver.com>
2025-07-20 02:25:08 +0900

    [YS-513] feat: 내정보수정 입력값 변경 감지 시 페이지 이탈할 경우 확인 모달

--- src/app/user/profile/mobile/edit/[infoType]/components/EditFormLayout/EditFormLayout.tsx
+++ src/app/user/profile/mobile/edit/[infoType]/components/EditFormLayout/EditFormLayout.tsx
@@ -4,6 +4,10 @@ import { ReactNode } from 'react';
 
 import TitleSection from '@/app/join/mobile/components/TitleSection/TitleSection';
 import { mainContentLayout } from './EditFormLayout.css';
+import ConfirmModal from '@/components/Modal/ConfirmModal/ConfirmModal';
+import { useFormContext } from 'react-hook-form';
+import useLeaveConfirmModal from '@/hooks/useLeaveConfirmModal';
+import { colors } from '@/styles/colors';
 
 interface EditFormLayoutProps {
   title: string;
@@ -13,10 +17,32 @@ interface EditFormLayoutProps {
 }
 
 const EditFormLayout = ({ title, description, children, emailBadge }: EditFormLayoutProps) => {
+  const form = useFormContext();
+  const { isLeaveConfirmModalOpen, handleCancelLeave, handleConfirmLeave } = useLeaveConfirmModal({
+    isUserInputDirty: form.formState.isDirty,
+  });
+
   return (
     <main className={mainContentLayout}>
       <TitleSection title={title} description={description} emailBadge={emailBadge} />
       <>{children}</>
+
+      {/* 내 정보 수정 중 이탈 시 */}
+      <ConfirmModal
+        isMobile
+        isOpen={isLeaveConfirmModalOpen}
+        onOpenChange={(open) => {
+          if (!open) {
+            handleCancelLeave();
+          }
+        }}
+        confirmTitle="페이지에서 나가시겠어요?"
+        descriptionText="입력한 내용은 따로 저장되지 않아요"
+        cancelText="취소"
+        confirmText="나가기"
+        confirmButtonColor={colors.primaryMint}
+        onConfirm={() => handleConfirmLeave({ goHome: false })}
+      />
     </main>
   );
 };
