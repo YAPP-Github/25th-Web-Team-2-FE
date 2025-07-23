fdc06f8bbeb7518d0faddecb49a5f413055bee1b
rbgksqkr <rbgks1937@naver.com>
2025-07-24 04:10:47 +0900

    [YS-479] fix: 모집 대상 popover 내부에서 저장하지 않을 경우 기존 필터링 정보 유지

---
+++ src/app/home/components/ExperimentPostContainer/FilterContainer/ContactTargetPopover/ContactTargetContent/ContactTargetContent.css.ts
@@ -0,0 +1,100 @@
+import { style } from '@vanilla-extract/css';
+
+import { colors } from '@/styles/colors';
+import { fonts } from '@/styles/fonts.css';
+
+export const genderSelectWrapper = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '0.8rem',
+});
+
+export const ageSelectWrapper = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '0.8rem',
+});
+
+export const labelWrapper = style({
+  display: 'flex',
+  gap: '0.4rem',
+  alignItems: 'center',
+});
+
+export const label = style({
+  ...fonts.body.normal.M16,
+  color: colors.text06,
+});
+
+export const subLabel = style({
+  ...fonts.label.large.R14,
+  color: colors.text03,
+});
+
+export const genderButtonGroup = style({
+  display: 'flex',
+  gap: '0.8rem',
+});
+
+export const genderButton = style({
+  ...fonts.label.large.M14,
+  color: colors.text06,
+  padding: '0.6rem 2.15rem',
+  borderRadius: '1.2rem',
+  outline: `1px solid ${colors.line01}`,
+  transition: 'color 0.1s, background-color 0.1s, outline 0.1s',
+  width: '10.6rem',
+  selectors: {
+    '&.active': {
+      backgroundColor: colors.primaryTinted,
+      outline: `0.1rem solid ${colors.lineTinted}`,
+      color: colors.textPrimary,
+    },
+  },
+});
+
+export const ageInputContainer = style({
+  display: 'flex',
+  gap: '1rem',
+  alignItems: 'center',
+});
+
+export const ageInput = style({
+  ...fonts.label.large.M14,
+  width: '100%',
+  padding: '0.6rem 0 0.6rem 1.6rem',
+  border: `0.1rem solid ${colors.line01}`,
+  borderRadius: '1.2rem',
+  selectors: {
+    '&:focus': {
+      outline: 'none',
+      border: `0.1rem solid ${colors.primaryMint}`,
+    },
+
+    '&::placeholder': {
+      color: colors.text02,
+    },
+  },
+});
+
+export const footerButtonContainer = style({
+  display: 'flex',
+  gap: '0.8rem',
+  justifyContent: 'flex-end',
+});
+
+export const resetButton = style({
+  ...fonts.label.large.SB14,
+  padding: '0.6rem 1.4rem',
+  borderRadius: '1.2rem',
+  backgroundColor: colors.field03,
+  color: colors.text06,
+});
+
+export const saveButton = style({
+  ...fonts.label.large.SB14,
+  padding: '0.6rem 1.4rem',
+  borderRadius: '1.2rem',
+  backgroundColor: colors.primaryMint,
+  color: colors.text01,
+});
--- src/app/home/components/ExperimentPostContainer/FilterContainer/ContactTargetPopover/ContactTargetFilter.tsx
+++ src/app/home/components/ExperimentPostContainer/FilterContainer/ContactTargetPopover/ContactTargetFilter.tsx
@@ -2,33 +2,17 @@
 
 import * as Popover from '@radix-ui/react-popover';
 import { assignInlineVars } from '@vanilla-extract/dynamic';
-import { ChangeEvent, useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import ContactTargetBottomSheet from './ContactTargetBottomSheet/ContactTargetBottomSheet';
-import {
-  genderSelectWrapper,
-  ageSelectWrapper,
-  popoverTrigger,
-  popoverContent,
-  labelWrapper,
-  label,
-  ageInputContainer,
-  footerButtonContainer,
-  resetButton,
-  saveButton,
-  ageInput,
-  genderButton,
-  genderButtonGroup,
-} from './ContactTargetFilter.css';
+import ContactTargetContent from './ContactTargetContent/ContactTargetContent';
+import { popoverContent, popoverTrigger } from './ContactTargetFilter.css';
 
-import { GENDER } from '@/app/home/home.constants';
 import { GenderFilterValue } from '@/app/home/home.types';
 import { getContactTargetFilterText, getFilterColors } from '@/app/home/home.utils';
 import Icon from '@/components/Icon';
 import useOverlay from '@/hooks/useOverlay';
 import { ExperimentPostListFilterParams } from '@/types/filter';
-
-const AGE_MAX_LENGTH = 3;
 
 interface ContactTargetFilterProps {
   onChange: (filters: ExperimentPostListFilterParams) => void;
@@ -41,19 +25,13 @@ const ContactTargetFilter = ({ onChange, filterGender, filterAge }: ContactTarge
   const [isOpen, setIsOpen] = useState(false);
   const isSelected = Boolean(filterAge) || Boolean(filterGender);
 
-  const [filteredGender, setFilteredGender] = useState<GenderFilterValue | null>(
-    filterGender || null,
-  );
-  const [filteredAge, setFilteredAge] = useState<string | null>(filterAge?.toString() || null);
-
   const handleOpenBottomSheet = (e: React.TouchEvent) => {
     e.preventDefault();
     open(
       () => (
         <ContactTargetBottomSheet
-          initialGender={filteredGender}
-          initialAge={filteredAge}
-          onReset={handleResetFilter}
+          initialGender={filterGender || null}
+          initialAge={filterAge?.toString() || ''}
           onChange={onChange}
           onClose={close}
         />
@@ -62,36 +40,8 @@ const ContactTargetFilter = ({ onChange, filterGender, filterAge }: ContactTarge
     );
   };
 
-  const handleChangeFilteredAge = (e: ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    const isValidNumber = /^\d*$/.test(value);
-
-    if (isValidNumber) {
-      setFilteredAge(value);
-    }
-  };
-
-  const handleResetFilter = () => {
-    setFilteredGender(null);
-    setFilteredAge(null);
-  };
-
-  const handleSaveFilter = () => {
-    setIsOpen(false);
-
-    onChange({
-      gender: filteredGender,
-      age: filteredAge !== null ? Number(filteredAge) : null,
-    });
-  };
-
-  useEffect(() => {
-    setFilteredGender(filterGender || null);
-    setFilteredAge(filterAge?.toString() || '');
-  }, [filterGender, filterAge]);
-
   return (
-    <Popover.Root open={isOpen} onOpenChange={() => setIsOpen((prev) => !prev)}>
+    <Popover.Root open={isOpen} onOpenChange={setIsOpen}>
       <Popover.Trigger
         className={popoverTrigger}
         style={assignInlineVars(getFilterColors(isSelected))}
@@ -102,43 +52,12 @@ const ContactTargetFilter = ({ onChange, filterGender, filterAge }: ContactTarge
       </Popover.Trigger>
       <Popover.Portal>
         <Popover.Content className={popoverContent} align="start">
-          <div className={genderSelectWrapper}>
-            <span className={label}>성별</span>
-            <div className={genderButtonGroup}>
-              {GENDER.map((option) => (
-                <button
-                  key={option.value}
-                  className={`${genderButton} ${option.value === filteredGender ? 'active' : ''}`}
-                  onClick={() => setFilteredGender(option.value)}
-                >
-                  {option.label}
-                </button>
-              ))}
-            </div>
-          </div>
-          <div className={ageSelectWrapper}>
-            <div className={labelWrapper}>
-              <span className={label}>나이</span>
-            </div>
-            <div className={ageInputContainer}>
-              <input
-                className={ageInput}
-                value={filteredAge ?? ''}
-                type="text"
-                maxLength={AGE_MAX_LENGTH}
-                onChange={handleChangeFilteredAge}
-                placeholder="만 나이 입력"
-              />
-            </div>
-          </div>
-          <div className={footerButtonContainer}>
-            <button onClick={handleResetFilter} className={resetButton}>
-              초기화
-            </button>
-            <button onClick={handleSaveFilter} className={saveButton}>
-              저장
-            </button>
-          </div>
+          <ContactTargetContent
+            initialGender={filterGender}
+            initialAge={filterAge}
+            onChange={onChange}
+            onClose={() => setIsOpen(false)}
+          />
         </Popover.Content>
       </Popover.Portal>
     </Popover.Root>
