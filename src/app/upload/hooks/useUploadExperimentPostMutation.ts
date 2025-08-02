49509aa09cb97960daac8c5ef0deeb171082258d
rbgksqkr <rbgks1937@naver.com>
2025-08-03 03:06:29 +0900

    [YS-531] fix: 업로드 후 내가 쓴 글과 공고 목록 캐시 무효화 처리

--- src/app/upload/hooks/useUploadExperimentPostMutation.ts
+++ src/app/upload/hooks/useUploadExperimentPostMutation.ts
@@ -1,10 +1,11 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { CustomError } from '@/apis/config/error';
 import { fetchClient } from '@/apis/config/fetchClient';
 import { GenderType } from '@/app/upload/components/ApplyMethodSection/ApplyMethodSection';
 import { API_URL } from '@/constants/url';
 import { MatchType } from '@/types/uploadExperimentPost';
+import { queryKey } from '@/constants/queryKey';
 
 export interface ExperimentPostData {
   startDate?: string | null;
@@ -64,6 +65,7 @@ export interface UseUploadExperimentPostMutationResponse {
 }
 
 const useUploadExperimentPostMutation = () => {
+  const queryClient = useQueryClient();
   const mutationKey = API_URL.uploadPost;
   const mutationFn = async (data: ExperimentPostData) =>
     await fetchClient.post<UseUploadExperimentPostMutationResponse>(mutationKey, {
@@ -73,6 +75,10 @@ const useUploadExperimentPostMutation = () => {
   return useMutation<UseUploadExperimentPostMutationResponse, CustomError, ExperimentPostData>({
     mutationKey: [mutationKey],
     mutationFn,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: queryKey.post.all });
+      queryClient.invalidateQueries({ queryKey: queryKey.myPosts.all });
+    },
   });
 };
 
