a25d12ec3affa97d3fbe62006abc82c22dcb16c7
rbgksqkr <rbgks1937@naver.com>
2025-08-03 00:00:33 +0900

    [YS-531] feat: 모바일 내가 쓴 글 UI 구현

---
+++ src/app/user/profile/mobile/components/MobileProfileSection/ResearcherProfileTab/MobileMyPosts/MobileMyPosts.css.ts
@@ -0,0 +1,89 @@
+import { style } from '@vanilla-extract/css';
+
+import { colors } from '@/styles/colors';
+import { fonts } from '@/styles/fonts.css';
+
+export const myPostsLayout = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '3.2rem',
+});
+
+export const listItem = style({
+  display: 'grid',
+  gap: '0.8rem',
+  gridTemplateAreas: `
+  "views menu"
+  "content content"
+  `,
+
+  position: 'relative',
+  selectors: {
+    '&::after': {
+      position: 'absolute',
+      content: '""',
+      width: '100%',
+      height: '0.1rem',
+      bottom: '-1.6rem',
+      left: '0',
+      backgroundColor: colors.line01,
+    },
+    '&:last-child::after': {
+      display: 'none',
+    },
+  },
+});
+
+export const viewsArea = style({
+  gridArea: 'views',
+  display: 'flex',
+  alignItems: 'center',
+  gap: '0.4rem',
+});
+
+export const postViews = style({
+  ...fonts.label.large.R14,
+  color: colors.text03,
+});
+
+export const viewsText = style({
+  fontSize: '14px',
+  color: '#666',
+});
+
+export const menuArea = style({
+  gridArea: 'menu',
+  justifySelf: 'end',
+});
+
+export const contentArea = style({
+  gridArea: 'content',
+});
+
+export const contentWrapper = style({
+  display: 'flex',
+  alignItems: 'center',
+  gap: '0.8rem',
+  flexWrap: 'nowrap',
+});
+
+export const recruitStatusBadge = style({
+  ...fonts.label.small.SB12,
+  color: colors.text05,
+  backgroundColor: colors.field04,
+  padding: '0.6rem 0.85rem',
+  borderRadius: '3rem',
+  whiteSpace: 'nowrap',
+});
+
+export const postTitle = style({
+  ...fonts.body.normal.M16,
+  color: colors.text06,
+
+  display: '-webkit-box',
+  WebkitLineClamp: 1,
+  WebkitBoxOrient: 'vertical',
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+  wordBreak: 'break-word',
+});
--- src/app/user/profile/mobile/components/MobileProfileSection/ResearcherProfileTab/MobileMyPosts/MobileMyPosts.tsx
+++ src/app/user/profile/mobile/components/MobileProfileSection/ResearcherProfileTab/MobileMyPosts/MobileMyPosts.tsx
@@ -1,5 +1,49 @@
+import Link from 'next/link';
+
+import {
+  contentArea,
+  contentWrapper,
+  listItem,
+  menuArea,
+  myPostsLayout,
+  postTitle,
+  postViews,
+  recruitStatusBadge,
+  viewsArea,
+} from './MobileMyPosts.css';
+
+import useMyPostsQuery from '@/app/my-posts/hooks/useMyPostsQuery';
+import Icon from '@/components/Icon';
+import { colors } from '@/styles/colors';
+
 const MobileMyPosts = () => {
-  return <div>내가 쓴 글 리스트</div>;
+  const { data } = useMyPostsQuery();
+
+  const posts = data?.content ?? [];
+
+  return (
+    <ul className={myPostsLayout}>
+      {posts.map((post) => (
+        <li key={post.experimentPostId} className={listItem}>
+          <div className={viewsArea}>
+            <Icon icon="Eye" width={18} height={18} color={colors.icon02} />
+            <span className={postViews}>{post.views}</span>
+          </div>
+
+          <button className={menuArea}>
+            <Icon icon="AllMenu" width={20} height={20} />
+          </button>
+
+          <Link href={`/post/${post.experimentPostId}`} className={contentArea}>
+            <div className={contentWrapper}>
+              {post.recruitStatus && <span className={recruitStatusBadge}>모집 완료</span>}
+              <span className={postTitle}>{post.title}</span>
+            </div>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default MobileMyPosts;
