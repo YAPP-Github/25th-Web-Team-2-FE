9e8b920b4326d5c9cd9376b71adbe255b9162780
rbgksqkr <rbgks1937@naver.com>
2025-08-03 11:20:23 +0900

    [YS-531] feat: 모집 중 서버 상태 관리에 낙관적 업데이트 적용

--- src/app/my-posts/hooks/useUpdateRecruitStatusMutation.ts
+++ src/app/my-posts/hooks/useUpdateRecruitStatusMutation.ts
@@ -1,8 +1,9 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { fetchClient } from '@/apis/config/fetchClient';
 import { queryKey } from '@/constants/queryKey';
 import { API_URL } from '@/constants/url';
+import { UseMyPostsQueryResponse } from './useMyPostsQuery';
 
 export interface UseUpdateRecruitStatusMutationParams {
   postId: string;
@@ -18,6 +19,8 @@ export interface UseUpdateRecruitStatusMutationResponse {
 }
 
 const useUpdateRecruitStatusMutation = () => {
+  const queryClient = useQueryClient();
+
   const mutationFn = async ({ postId }: UseUpdateRecruitStatusMutationParams) => {
     const url = API_URL.updateRecruitStatus(postId);
     return await fetchClient.patch<UseUpdateRecruitStatusMutationResponse>(url);
@@ -26,6 +29,32 @@ const useUpdateRecruitStatusMutation = () => {
   return useMutation({
     mutationKey: queryKey.updateRecruitStatus,
     mutationFn,
+    onSuccess: ({ experimentPostId }) => {
+      queryClient.invalidateQueries({ queryKey: queryKey.post.all });
+      queryClient.invalidateQueries({ queryKey: queryKey.experimentPostDetail(experimentPostId) });
+      queryClient.invalidateQueries({ queryKey: queryKey.myPosts.all });
+    },
+    onMutate: async ({ postId }) => {
+      const previousData = queryClient.getQueryData<UseMyPostsQueryResponse>(queryKey.myPosts.all);
+
+      queryClient.setQueryData<UseMyPostsQueryResponse>(queryKey.myPosts.all, (oldData) => {
+        if (!oldData) return oldData;
+
+        return {
+          ...oldData,
+          content: oldData.content.map((post) =>
+            post.experimentPostId === postId ? { ...post, recruitStatus: false } : post,
+          ),
+        };
+      });
+
+      return { previousData };
+    },
+    onError: (_, __, context) => {
+      if (context?.previousData) {
+        queryClient.setQueryData(queryKey.myPosts.all, context.previousData);
+      }
+    },
   });
 };
 
