20be35f0e8dfbab1d9837f19a6293ce6822cec35
eeeyooon <vywns9978@gmail.com>
2025-08-06 17:15:34 +0900

    [YS-537] feat: 모바일 공고 상세 error 컴포넌트 추가

---
+++ src/app/post/[postId]/mobile/error.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+import { experimentPostMobileHeaderLayout } from './components/ExperimentPostMobileHeader/ExperimentPostMobileHeader.css';
+
+import { commonStatusLayout, commonStatusText } from '@/app/common-status.css';
+import RightHeader from '@/components/Header/RightHeader/RightHeader';
+import Icon from '@/components/Icon';
+import MobilePostDetailLayout from '@/components/layout/MobilePostDetailLayout/MobilePostDetailLayout';
+import { colors } from '@/styles/colors';
+
+export default function Error() {
+  const router = useRouter();
+
+  const handleGoBack = () => {
+    if (window.history.length > 1) {
+      router.back();
+    } else {
+      router.push('/');
+    }
+  };
+  return (
+    <MobilePostDetailLayout>
+      <div className={experimentPostMobileHeaderLayout}>
+        <button onClick={handleGoBack} aria-label="뒤로가기">
+          <Icon icon="Arrow" width={24} height={24} color={colors.text06} />
+        </button>
+        <RightHeader />
+      </div>
+      <div className="min-h-screen flex items-center justify-center">
+        <div className={commonStatusLayout}>
+          <p className={commonStatusText}>
+            공고 정보를 불러오지 못했습니다. <br /> 잠시 후 다시 시도해주세요.
+          </p>
+        </div>
+      </div>
+    </MobilePostDetailLayout>
+  );
+}
