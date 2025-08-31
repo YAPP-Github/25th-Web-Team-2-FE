5f99c065eeb4fe82b8e5c475f0bacdc35ffece76
rbgksqkr <rbgks1937@naver.com>
2025-08-31 23:30:03 +0900

    [YS-452] feat: log 함수를 만들어 sentry로 에러 추적

---
+++ src/lib/log.ts
@@ -0,0 +1,55 @@
+import * as Sentry from '@sentry/nextjs';
+
+interface LogAPIErrorProps {
+  level: Sentry.SeverityLevel;
+  errorCode: string;
+  httpStatus: number;
+  url: string;
+  errorMessage: string;
+}
+
+interface LogNetworkErrorProps {
+  url: string;
+}
+
+interface LogUnhandledErrorProps {
+  url: string;
+}
+
+export const logAPIError = ({
+  level,
+  errorCode,
+  httpStatus,
+  url,
+  errorMessage,
+}: LogAPIErrorProps) => {
+  Sentry.withScope((scope) => {
+    scope.setLevel(level);
+    scope.setTag('api', 'apiError');
+    scope.setTag('errorCode', errorCode);
+    scope.setTag('httpStatus', httpStatus.toString());
+    scope.setTag('endpoint', url);
+
+    Sentry.captureException(new Error(errorMessage));
+  });
+};
+
+export const logNetworkError = ({ url }: LogNetworkErrorProps) => {
+  Sentry.withScope((scope) => {
+    scope.setLevel('warning');
+    scope.setTag('api', 'networkError');
+    scope.setTag('endpoint', url);
+
+    Sentry.captureException(new Error('네트워크가 불안정해요. 다시 시도해주세요!'));
+  });
+};
+
+export const logUnhandledError = ({ url }: LogUnhandledErrorProps) => {
+  Sentry.withScope((scope) => {
+    scope.setLevel('fatal');
+    scope.setTag('api', 'unhandledError');
+    scope.setTag('endpoint', url);
+
+    Sentry.captureException(new Error('예기치 못한 에러가 발생했어요. 관리자에게 문의 바랍니다.'));
+  });
+};
