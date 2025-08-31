5ee4208cc74ee022e34194e0d6fd1af3620e0dce
rbgksqkr <rbgks1937@naver.com>
2025-08-31 22:55:12 +0900

    [YS-452] feat: sentry/nextjs 스캐폴드 적용

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-
+import { withSentryConfig } from '@sentry/nextjs';
 import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
 
 const nextConfig = {
@@ -34,17 +34,25 @@ const nextConfig = {
     }
     return config;
   },
-  async redirects() {
-    return [
-      {
-        source: '/error',
-        destination: '/',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 const withVanillaExtract = createVanillaExtractPlugin();
 
-export default withVanillaExtract(nextConfig);
+// https://www.npmjs.com/package/@sentry/webpack-plugin#options
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+export default withSentryConfig(withVanillaExtract(nextConfig), {
+  org: 'gradmeet',
+  project: 'gradmeet-front',
+
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
+
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
+
+  // ad-blocker 방지를 위해 사용 (서버 비용 증가 시 제거)
+  tunnelRoute: true,
+
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
+});
