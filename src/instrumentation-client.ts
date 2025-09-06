58c0d4a0230ac7da8e4d144ebafe8696ca0d9af1
rbgksqkr <rbgks1937@naver.com>
2025-09-06 17:37:54 +0900

    [YS-550] chore: 주석 제거

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -5,13 +5,8 @@ const isProductionDomain = process.env.VERCEL_ENV === 'production';
 if (isProductionDomain) {
   Sentry.init({
     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-
-    // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
     tracesSampleRate: 1,
-    // Enable logs to be sent to Sentry
     enableLogs: true,
-
-    // Setting this option to true will print useful information to the console while you're setting up Sentry.
     debug: false,
   });
 }
