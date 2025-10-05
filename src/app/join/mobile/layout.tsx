1b912d60f2064c3936a58044e86fef95a7a714b6
rbgksqkr <rbgks1937@naver.com>
2025-10-05 14:09:41 +0900

    [YS-558] fix: input 자동 줌인 제어하는 viewport 설정 추가

--- src/app/join/mobile/layout.tsx
+++ src/app/join/mobile/layout.tsx
@@ -1,4 +1,13 @@
+import type { Viewport } from 'next';
+
 import { mobileJoinPageLayout } from '../JoinPage.css';
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  // maximumScale: 1,
+  // userScalable: false,
+};
 
 export default function MobileJoinLayout({ children }: { children: React.ReactNode }) {
   return <div className={mobileJoinPageLayout}>{children}</div>;
