7b7d5d259982b28adb90eef4d4b188b8cb8c3011
rbgksqkr <rbgks1937@naver.com>
2026-03-01 17:19:17 +0900

    refactor: eventName 변수 분리

--- src/app/login/LoginPage.utils.ts
+++ src/app/login/LoginPage.utils.ts
@@ -21,14 +21,13 @@ export const getAuthErrorMessage = (role: string, error: CustomError) => {
 export const getOAuthLoginConfig = (role: Role) => {
   const googleOauthUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI}&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email&state=${role}|${LOGIN_PROVIDER.google}`;
   const naverOauthUrl = `https://nid.naver.com/oauth2.0/authorize?client_id=${process.env.NEXT_PUBLIC_NAVER_CLIENT_ID}&response_type=code&redirect_uri=${process.env.NEXT_PUBLIC_NAVER_REDIRECT_URI}&state=${process.env.NEXT_PUBLIC_NAVER_STATE}|${role}|${LOGIN_PROVIDER.naver}`;
+  const eventName = role === ROLE.researcher ? CLICK_LOGIN_RESEARCHER : CLICK_LOGIN_PARTICIPANT;
 
   const handleGoogleLogin = () => {
-    const eventName = role === ROLE.researcher ? CLICK_LOGIN_RESEARCHER : CLICK_LOGIN_PARTICIPANT;
     trackEvent(eventName, { provider: LOGIN_PROVIDER.google });
   };
 
   const handleNaverLogin = () => {
-    const eventName = role === ROLE.researcher ? CLICK_LOGIN_RESEARCHER : CLICK_LOGIN_PARTICIPANT;
     trackEvent(eventName, { provider: LOGIN_PROVIDER.naver });
   };
 
